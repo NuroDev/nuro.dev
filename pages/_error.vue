77bb5639d0e6e40a933b6e21a8ccc48f49fd85ba
[MEADOW_DEV] <bendixon50@gmail.com>
2018-06-15 14:21:32 +0100

    :art: Error page structure update

--- pages/_error.vue
+++ pages/_error.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-jumbotron class='contentContainer elevation-0'>
+  <v-jumbotron class='contentContainer elevation-0' style='height: 100vh;!important'>
     <v-container fill-height text-xs-center>
       <v-layout row wrap align-center>
-        <v-flex xs12 sm12 md12 lg12 xl12 class='error'>
+        <v-flex xs12 sm12 md12 lg12 xl12>
 
           <h1 class='errorTitle'>⚠️ ERROR! ⚠️</h1>
           <p class='errorSubTitle'>Sadly the page you were  <br /> looking for could not be found... </p>
 
-          <v-btn flat large color='grey darken-3' to='/'>
+          <v-btn flat large round color='grey darken-3' to='/'>
             <v-icon left>arrow_back</v-icon>
             <span>Back to Home</span>
           </v-btn>
@@ -19,22 +19,12 @@
 </template>
 
 <style>
-  .error {
-    font-family: 'Roboto', Arial, Helvetica, sans-serif;
-    font-weight: 300;
-    text-transform: uppercase;
-    letter-spacing: .20em;
-    background-color: transparent!important;
-  }
   .errorTitle {
     font-size: 42px;
     color: #424242;
   }
   .errorSubTitle {
     font-size: 16px;
-    color: #424242;
-  }
-  .errorBtn {
     color: #424242;
   }
   @media screen and (min-width: 768px) {
