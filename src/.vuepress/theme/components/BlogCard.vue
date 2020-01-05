f1adb499a139cbcbd2bd32d758dd18b13c9cfe0f
N U R O ™ <me@nuro.sh>
2020-01-05 00:47:38 +0000

    :art: Minor code cleanup

--- src/.vuepress/theme/components/BlogCard.vue
+++ src/.vuepress/theme/components/BlogCard.vue
@@ -1,27 +1,16 @@
 <template>
-	<!-- <article class="md:rounded-lg shadow-md hover:shadow-2xl defaultTransition p-8 md:p-16 mb-0 md:mb-12 -mt-1 md:-mt-0 bg-white">
+	<article class="md:rounded-lg shadow-md hover:shadow-2xl defaultTransition p-8 md:p-16 mb-0 md:mb-12 -mt-1 md:-mt-0 bg-white">
 		<div class="py-4">
 			<span v-if="tag != undefined" class="inline-block bg-gray-200 rounded-full px-3 py-1 text-gray-700 mr-2 mb-5 uppercase tracking-widest text-xs tag-notes" v-text="`#${tag}`" />
-			<a :href="url" class="text-2xl md:text-4xl mb-4 block font-semibold leading-tight text-gray-800 hover:text-gray-700">
-                Do less but focus
-            </a>
+			<a :href="url" class="text-2xl md:text-4xl mb-4 block font-semibold leading-tight text-gray-800 hover:text-gray-700" v-text="title" />
 			<p class="text-gray-500" v-text="date" />
-			<p class="text-gray-700 text-base"></p>
 			<a :href="url" class="mt-6 block text-gray-600 hover:text-gray-700 hover:underline">
 				Read more
+				<!-- TODO: Add SVG icon -->
 				<i class="pl-2 fas fa-arrow-right"></i>
 			</a>
 		</div>
-	</article> -->
-    <article class="flex-1 mx-auto leading-normal container">
-      <h2>{{$page.frontmatter.title}}</h2>
-      <label class="text-grey-dark">
-        <span>{{$page.readingTime.text}}</span> •
-        <span>{{formateDate($page.frontmatter.date)}}</span>
-      </label>
-
-      <Content/>
-    </article>
+	</article>
 </template>
 
 <script>
@@ -29,28 +18,25 @@ export default {
 	props: {
 		date: {
             type: Date,
-			// required: true,
+			required: true,
 		},
 		description: {
 			type: String,
-			// required: true,
+			required: true,
 		},
 		tag: {
 			type: String,
-			// required: true,
+			required: true,
 		},
 		title: {
 			type: String,
-			// required: true,
+			required: true,
 		},
 		url: {
 			type: String,
 			default: '/',
-			// required: true,
+			required: true,
 		},
-    },
-    mounted () {
-        console.log(this.$page);
     }
 };
 </script>
--- src/.vuepress/theme/components/Navbar.vue
+++ src/.vuepress/theme/components/Navbar.vue
@@ -1,11 +1,6 @@
 <template>
 	<nav class="w-full pt-8">
-		<div class="flex items-center justify-between max-w-6xl mx-auto">
-			<ul class="flex items-center">
-				<li>
-					<img class="rounded-full h-10 w-10 bg-gray-900 block avatar" src="@theme/assets/avatar.png" />
-				</li>
-			</ul>
+		<div class="flex items-center justify-center max-w-6xl mx-auto">
 			<ul class="flex">
 				<a href="/" class="p-2 px-4 mx-2 rounded font-bold uppercase tracking-widest text-gray-500 hover:text-gray-600 hover:bg-gray-300 defaultTransition cursor-pointer">home</a>
 				<a href="/blog" class="p-2 px-4 mx-2 rounded font-bold uppercase tracking-widest text-gray-500 hover:text-gray-600 hover:bg-gray-300 defaultTransition cursor-pointer">blog</a>
@@ -14,8 +9,3 @@
 		</div>
 	</nav>
 </template>
-
-<script>
-export default {
-};
-</script>
