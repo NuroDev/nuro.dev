e72fe89cbbbb08a8a02040185ccea73e487fddd7
N U R O ™ <me@nuro.sh>
2020-01-05 01:13:47 +0000

    :construction: Temp fixed posts loading

--- src/.vuepress/theme/layouts/BlogLayout.vue
+++ src/.vuepress/theme/layouts/BlogLayout.vue
@@ -2,15 +2,15 @@
 	<div class="flex flex-col h-full">
 		<Navbar />
 
-		<main class="container max-w-3xl mx-auto mt-16 flex flex-wrap items-center justify-around">
+		<main class="mt-16 flex flex-wrap items-center justify-around">
 			<BlogCard 
-				v-for="(post, i) in posts" 
+				v-for="(post, i) in 5" 
 				:key="i"
-				:title="post.frontmatter.title" 
-				:description="post.frontmatter.description" 
-				:date="formateDate(post.frontmatter.date)" 
-				:url="post.frontmatter.url" 
-				:tag="post.frontmatter.notes"
+				title="Title" 
+				description="Description" 
+				:date="new Date()" 
+				url="/"
+				tag="Random"
 			/>
 		</main>
 	</div>
@@ -33,7 +33,7 @@ export default {
 	computed: {
 		posts: () => {
 			// this.$site.pages.filter(x => x.path.startsWith('/blog/') && !x.frontmatter.index).sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
-			console.log(this.$site);
+			// console.log(this.$site);
 		}
 	}
 }
