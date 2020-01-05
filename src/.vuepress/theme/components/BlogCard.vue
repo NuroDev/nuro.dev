6cbf47f6ce2b66764b91c15eb949bbfd2e1f0dc4
N U R O ™ <me@nuro.sh>
2020-01-05 01:14:04 +0000

    :lipstick: Cleaned up blog card component

--- src/.vuepress/theme/components/BlogCard.vue
+++ src/.vuepress/theme/components/BlogCard.vue
@@ -1,10 +1,10 @@
 <template>
-	<article class="md:rounded-lg shadow-md hover:shadow-2xl defaultTransition p-8 md:p-16 mb-0 md:mb-12 -mt-1 md:-mt-0 bg-white">
+	<article class="rounded-lg shadow-md hover:shadow-2xl defaultTransition p-8 md:p-16 mx-4 mb-4 md:mb-12 bg-white">
 		<div class="py-4">
-			<span v-if="tag != undefined" class="inline-block bg-gray-200 rounded-full px-3 py-1 text-gray-700 mr-2 mb-5 uppercase tracking-widest text-xs tag-notes" v-text="`#${tag}`" />
+			<span v-if="tag != undefined" class="inline-block bg-blue-100 rounded-full px-3 py-1 text-blue-500 mr-2 mb-5 uppercase tracking-widest text-xs tag-notes" v-text="`#${tag}`" />
 			<a :href="url" class="text-2xl md:text-4xl mb-4 block font-semibold leading-tight text-gray-800 hover:text-gray-700" v-text="title" />
 			<p class="text-gray-500" v-text="date" />
-			<a :href="url" class="mt-6 block text-gray-600 hover:text-gray-700 hover:underline">
+			<a :href="url" class="mt-6 block text-gray-600 hover:text-gray-900 defaultTransition">
 				Read more
 				<!-- TODO: Add SVG icon -->
 				<i class="pl-2 fas fa-arrow-right"></i>
@@ -37,7 +37,7 @@ export default {
 			default: '/',
 			required: true,
 		},
-    }
+	},
 };
 </script>
 
