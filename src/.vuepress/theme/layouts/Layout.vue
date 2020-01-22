d6fc25c54983285455b3fcf4829a5c5f8c2b4c57
N U R O ™ <bendixon50@gmail.com>
2020-01-22 13:43:49 +0000

    :sparkles: Added typed.js for homepage

--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -3,12 +3,14 @@
 		<Navbar />
 
 		<main class="container h-screen max-w-full mx-auto flex flex-wrap items-center justify-around">
-			<h1 class="title">
+			<h1 class="title" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-out-back">
 				Hi! 👋🏻
 				<br />I'm Ben,
-				<br />a
-				<span>developer</span>
-				<br />based in the UK
+				<br />
+				<vue-typed-js :strings="['gamer', 'nerd', 'developer']" cursorChar=" " :startDelay="800" class="title">
+					<h1>a <span class="typing"></span></h1>
+				</vue-typed-js>
+				based in the UK
 			</h1>
 		</main>
 	</div>
