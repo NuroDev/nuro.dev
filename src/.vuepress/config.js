ea01234e4c4cda9b3ccd3a705fb6412f5d47546b
N U R O ™ <me@nuro.sh>
2020-07-19 15:46:53 +0100

    :bento: Added favicons/manifest files

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -19,4 +19,13 @@ module.exports = {
 			updatePopup: true,
 		},
 	},
+	head: [
+		['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"}],
+		['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"}],
+		['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"}],
+		['link', { rel: "manifest", href: "/site.webmanifest"}],
+		['link', { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#0072ff"}],
+		['link', { rel: "shortcut icon", href: "/favicon.ico"}],
+		['meta', { name: "theme-color", content: "#0072ff"}],
+	],
 };
