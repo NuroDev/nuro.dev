447d91222421d9164c0f86ceb3760dcaac6b4b70
N U R O ™ <bendixon50@gmail.com>
2020-10-16 10:29:43 +0100

    :fire: Removed header image

--- components/Header.vue
+++ components/Header.vue
@@ -1,18 +1,9 @@
 <template>
-	<section class="relative overflow-hidden bg-white dark:bg-gray-800 shadow dark:shadow-none">
+	<section class="bg-white dark:bg-gray-800 shadow dark:shadow-none">
 		<div class="max-w-screen-xl mx-auto">
 			<div
-				class="relative z-10 pb-8 bg-white dark:bg-gray-800 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 py-16 pb-32"
+				class="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 py-16 pb-32"
 			>
-				<svg
-					class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white dark:text-gray-800 transform translate-x-1/2"
-					fill="currentColor"
-					viewBox="0 0 100 100"
-					preserveAspectRatio="none"
-				>
-					<polygon points="50,0 100,0 50,100 0,100" />
-				</svg>
-
 				<div class="relative pt-6 px-4 sm:px-6 lg:px-8">
 					<nav
 						class="relative flex items-center justify-between sm:h-10 lg:justify-start"
@@ -213,13 +204,6 @@
 					</div>
 				</main>
 			</div>
-		</div>
-		<div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-			<img
-				class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-				src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-				alt=""
-			/>
 		</div>
 	</section>
 </template>
