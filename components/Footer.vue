46e33166bde447f23d5c1b44b38212a6f79274d1
N U R O ™ <bendixon50@gmail.com>
2020-10-17 05:34:06 +0100

    :sparkles: Updated footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,118 +1,62 @@
 <template>
-	<footer class="px-8 w-full py-8 bg-gray-100 dark:bg-gray-900">
-		<div
-			class="mx-auto max-w-screen-xl grid grid-cols-4 md:grid-cols-6 lg:grid-cols-10 gap-10 lg:gap-20 mb-3"
-		>
-			<div class="col-span-4 md:col-span-3">
-				<a href="/">
-					<span class="flex items-center">
-						<h1 class="ml-3 text-3xl font-semibold text-red-600">Title</h1>
-					</span>
-				</a>
-				<p class="text-md my-4 text-gray-500">
-					Data to enrich your online business<br /><br />
-
-					John Oliver<br />Oxford Street<br />London<br />United Kingdom<br />
-				</p>
-			</div>
-			<nav class="col-span-1">
-				<p
-					class="uppercase text-gray-900 dark:text-white text-xs tracking-wider font-medium mb-3"
-				>
-					Product
-				</p>
+	<footer class="bg-gray-100 dark:bg-gray-900 body-font">
+		<div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+			<a
+				class="flex title-font font-medium items-center md:justify-start justify-center text-white"
+			>
+				<img
+					src="/icon.png"
+					alt="Avatar"
+					class="w-10 h-10 text-white p-2 bg-red-600 rounded-full"
+				/>
+			</a>
+			<p
+				class="text-sm text-gray-600 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:dark:border-gray-700 sm:py-2 sm:mt-0 mt-4"
+			>
+				© 2020 —
 				<a
-					class="flex mb-3 md:mb-2 text-sm font-medium text-gray-500 hover:text-gray-600 transition-colors duration-100 ease-in"
-					>Features</a
-				><a
-					class="flex mb-3 md:mb-2 text-sm font-medium text-gray-500 hover:text-gray-600 transition-colors duration-100 ease-in"
-					>Sign Up</a
-				><a
-					class="flex mb-3 md:mb-2 text-sm font-medium text-gray-500 hover:text-gray-600 transition-colors duration-100 ease-in"
-					>Roadmap</a
-				><a
-					class="flex mb-3 md:mb-2 text-sm font-medium text-gray-500 hover:text-gray-600 transition-colors duration-100 ease-in"
-					>Documentation</a
-				><a
-					class="flex mb-3 md:mb-2 text-sm font-medium text-gray-500 hover:text-gray-600 transition-colors duration-100 ease-in"
-					>Pricing</a
+					href="https://twitter.com/knyttneve"
+					class="text-gray-500 ml-1"
+					target="_blank"
+					rel="noopener noreferrer"
+					>@knyttneve</a
 				>
-			</nav>
-			<nav class="col-span-1">
-				<p
-					class="uppercase text-gray-900 dark:text-white text-xs tracking-wider font-medium mb-3"
-				>
-					Contact
-				</p>
-				<a
-					class="flex mb-3 md:mb-2 text-sm font-medium text-gray-500 hover:text-gray-600 transition-colors duration-100 ease-in"
-					>Blog</a
-				><a
-					class="flex mb-3 md:mb-2 text-sm font-medium text-gray-500 hover:text-gray-600 transition-colors duration-100 ease-in"
-					>Twitter</a
-				><a
-					class="flex mb-3 md:mb-2 text-sm font-medium text-gray-500 hover:text-gray-600 transition-colors duration-100 ease-in"
-					>Email</a
-				>
-			</nav>
-			<nav class="col-span-1 lg:col-span-2">
-				<p
-					class="uppercase text-gray-900 dark:text-white text-xs tracking-wider font-medium mb-3"
-				>
-					Legal
-				</p>
-				<a
-					class="flex mb-3 md:mb-2 text-sm font-medium text-gray-500 hover:text-gray-600 transition-colors duration-100 ease-in"
-					>Terms of Use</a
-				><a
-					class="flex mb-3 md:mb-2 text-sm font-medium text-gray-500 hover:text-gray-600 transition-colors duration-100 ease-in"
-					>Privacy Policy</a
-				>
-			</nav>
-			<div class="col-span-3">
-				<p
-					class="uppercase text-gray-600 text-xs tracking-wider font-medium mb-3 uppercase"
-				>
-					Subscribe to our newsletter
-				</p>
-				<form class="mb-2">
-					<label class="tag-label tag-label-sm"
-						><div class="tag-append">
-							<input
-								type="email"
-								name="email"
-								class="inline-block placeholder-gray-500 appearance-none px-3 py-2 text-gray-900 rounded-l-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-								placeholder="Enter your email"
-								required=""
-							/><button
-								class="text-center px-4 py-1 border border-transparent text-base leading-6 font-medium rounded-r-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-indigo transition duration-150 ease-in-out"
-								type="submit"
-							>
-								Subscribe
-							</button>
-						</div></label
-					>
-				</form>
-				<p class="text-xs text-gray-600">
-					Stay up-to-date on development and get exclusive insights
-				</p>
-			</div>
-		</div>
-		<div
-			class="max-w-screen-xl mx-auto flex flex-col md:flex-row justify-between items-start md:items-center border-t border-gray-200 pt-10 mt-10"
-		>
-			<p class="text-gray-700 dark:text-gray-500 font-medium text-sm text-left mb-6 md:mb-0">
-				© Copyright 2020 John. All Rights Reserved
 			</p>
-			<div class="flex items-start md:items-center justify-start md:justify-center space-x-6">
-				<a
-					class="font-medium text-sm text-gray-700 dark:text-gray-500 hover:text-gray-600 transition-colors duration-100 ease-in"
-					>Terms</a
-				><a
-					class="font-medium text-sm text-gray-700 dark:text-gray-500 hover:text-gray-600 transition-colors duration-100 ease-in"
-					>Privacy</a
+			<span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+				<NuxtLink
+					to="/twitter"
+					class="ml-3 text-gray-500 hover:text-black dark:hover:text-white transition duration-300 ease-in-out hover:bg-gray-100 dark:hover:bg-gray-700 p-3 rounded-full"
 				>
-			</div>
+					<TwitterIcon size="1x" class="w-5 h-5" />
+				</NuxtLink>
+
+				<NuxtLink
+					to="/github"
+					class="ml-3 text-gray-500 hover:text-black dark:hover:text-white transition duration-300 ease-in-out hover:bg-gray-100 dark:hover:bg-gray-700 p-3 rounded-full"
+				>
+					<GithubIcon size="1x" class="w-5 h-5" />
+				</NuxtLink>
+
+				<NuxtLink
+					to="/linkedin"
+					class="ml-3 text-gray-500 hover:text-black dark:hover:text-white transition duration-300 ease-in-out hover:bg-gray-100 dark:hover:bg-gray-700 p-3 rounded-full"
+				>
+					<LinkedinIcon size="1x" class="w-5 h-5" />
+				</NuxtLink>
+			</span>
 		</div>
 	</footer>
 </template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+import { GithubIcon, LinkedinIcon, TwitterIcon } from 'vue-feather-icons';
+
+export default defineComponent({
+	components: {
+		GithubIcon,
+		LinkedinIcon,
+		TwitterIcon,
+	},
+});
+</script>
