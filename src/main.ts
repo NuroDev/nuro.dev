42894ccb775a9593e440ecd9713fa3b819a8598c
nurodev <me@nuro.dev>
2021-04-15 22:50:30 +0100

    Added @vueuse packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue';
+import { createHead } from '@vueuse/head';
 
 import 'virtual:windi.css';
 
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+const head = createHead();
+
+createApp(App).use(router).use(head).mount('#app');
