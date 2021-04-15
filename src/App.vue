1f3587281282f3afafd6c9f82e5afb1fbf6e56f0
nurodev <me@nuro.dev>
2021-04-15 22:16:43 +0100

    Switched to Vue 3 `setup` syntax

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,8 @@
 	<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
 import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-	name: 'App',
-	components: {
-		HelloWorld,
-	},
-});
 </script>
 
 <style lang="postcss">
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,70 +1,69 @@
 <template>
-  <h1>{{ msg }}</h1>
+	<h1>{{ msg }}</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a
-      href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
-      target="_blank"
-    >
-      Vetur
-    </a>
-    or
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    (if using
-    <code>&lt;script setup&gt;</code>)
-  </p>
+	<p>
+		Recommended IDE setup:
+		<a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+		+
+		<a href="https://marketplace.visualstudio.com/items?itemName=octref.vetur" target="_blank">
+			Vetur
+		</a>
+		or
+		<a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+		(if using
+		<code>&lt;script setup&gt;</code>)
+	</p>
 
-  <p>See <code>README.md</code> for more information.</p>
+	<p>See <code>README.md</code> for more information.</p>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
+	<p>
+		<a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
+		|
+		<a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+	</p>
 
-  <button @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+	<button @click="increment" class="button">count is: {{ count }}</button>
+	<p>
+		Edit
+		<code>components/HelloWorld.vue</code> to test hot module replacement.
+	</p>
 </template>
 
-<script lang="ts">
-import { ref, defineComponent } from 'vue'
-export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: {
-      type: String,
-      required: true
-    }
-  },
-  setup: () => {
-    const count = ref(0)
-    return { count }
-  }
-})
+<script lang="ts" setup>
+import { defineProps, ref } from 'vue';
+
+defineProps({
+	msg: {
+		type: String,
+		required: true,
+	},
+});
+
+const count = ref(0);
+const increment = () => count.value++;
 </script>
 
-<style scoped>
+<style lang="postcss" scoped>
 a {
-  color: #42b983;
+	color: #42b983;
 }
 
 label {
-  margin: 0 0.5em;
-  font-weight: bold;
+	margin: 0 0.5em;
+	font-weight: bold;
 }
 
 code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
+	background-color: #eee;
+	padding: 2px 4px;
+	border-radius: 4px;
+	color: #304455;
+}
+
+.button {
+	@apply m-8 px-8 py-2 rounded-md \
+  text-white bg-green-500 hover:bg-green-600 \
+  transition ease-in-out duration-300 \
+  focus:outline-none focus:ring-4 focus:ring-green-500 focus:ring-opacity-50;
 }
 </style>
