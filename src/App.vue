1939fba0168b3e13935f3e06f7d3eff7a8dd63a7
nurodev <me@nuro.dev>
2021-04-18 22:09:53 +0100

    Set global font to Inter font

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,28 @@ if (import.meta.env.PROD)
 </script>
 
 <style lang="postcss">
-#app {
-	@apply antialiased text-gray-500 dark:text-gray-400;
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-}
+@use "~inter-ui/default" with (
+  $inter-font-display: swap,
+  $inter-font-path: '~inter-ui/Inter (web)'
+);
+@use "~inter-ui/variable" with (
+  $inter-font-display: swap,
+  $inter-font-path: '~inter-ui/Inter (web)'
+);
+@include default.all;
+@include variable.all;
 
 html {
-	@apply bg-gray-50;
+	@apply antialiased bg-gray-50 text-gray-500 dark:text-gray-400;
+	font-family: 'Inter', 'system-ui';
 }
 html.dark {
 	@apply bg-gray-900;
+}
+
+@supports (font-variation-settings: normal) {
+	html {
+		font-family: 'Inter var', 'system-ui';
+	}
 }
 </style>
