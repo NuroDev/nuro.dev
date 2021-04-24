01e156d9ec038bcce6e1bccfe19a333e67017360
nurodev <me@nuro.dev>
2021-04-24 07:20:14 +0100

    Updated theme component button styles

--- src/components/ThemeButton.vue
+++ src/components/ThemeButton.vue
@@ -20,13 +20,15 @@ const toggleDark = useToggle(isDark);
 
 <style lang="postcss" scoped>
 .themeButton {
-	@apply w-11 h-11 p-4 rounded-full \
-		text-gray-500 dark:text-gray-200 \
-		border border-gray-300 dark:border-gray-500 \
+	@apply flex justify-center items-center w-12 h-12 p-2 rounded-lg \
+		text-gray-300 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-300 \
+		bg-gray-100 hover:bg-gray-200 hover:bg-opacity-50 dark:bg-gray-800 dark:hover:bg-gray-700 \
 		transition ease-in-out duration-300 \
-		focus:outline-none focus:ring-4 focus:ring-primary-500 focus:text-primary-500;
+		focus:outline-none focus-visible:ring-4 focus-visible:ring-white focus-visible:ring-opacity-75;
+	/* transform translate-y-0 hover:-translate-y-1 active:translate-y-1; */
+
 	svg {
-		@apply relative -top-1 -left-1;
+		@apply w-6 h-6;
 	}
 }
 </style>
