c61daf6e2def460be49212e6831428f5a4c08e0e
nurodev <me@nuro.dev>
2021-04-24 06:14:39 +0100

    Cleaned up HelloWorld counter component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,49 +1,24 @@
 <template>
-	<h1>{{ msg }}</h1>
+	<div class="counter">
+		<Clickable class="inline-flex">
+			<button @click="decrement" class="button">
+				<i-feather-arrow-down class="mr-2 w-4" />
+			</button>
+		</Clickable>
 
-	<p>
-		Recommended IDE setup:
-		<a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-		+
-		<a href="https://marketplace.visualstudio.com/items?itemName=octref.vetur" target="_blank">
-			Vetur
-		</a>
-		or
-		<a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-		(if using
-		<code>&lt;script setup&gt;</code>)
-	</p>
+		<span>count is: {{ count }}</span>
 
-	<p>See <code>README.md</code> for more information.</p>
-
-	<p>
-		<a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
-		|
-		<a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-	</p>
-
-	<Clickable class="inline-flex">
-		<button @click="decrement" class="button">
-			<i-feather-arrow-down class="mr-2 w-4" />
-		</button>
-	</Clickable>
-
-	<span>count is: {{ count }}</span>
-
-	<Clickable class="inline-flex">
-		<button @click="increment" class="button">
-			<i-feather-arrow-up class="mr-2 w-4" />
-		</button>
-	</Clickable>
-	<p>
-		Edit
-		<code>components/HelloWorld.vue</code> to test hot module replacement.
-	</p>
+		<Clickable class="inline-flex">
+			<button @click="increment" class="button">
+				<i-feather-arrow-up class="mr-2 w-4" />
+			</button>
+		</Clickable>
+	</div>
 </template>
 
 <script lang="ts" setup>
-import { useStorage, useToggle } from '@vueuse/core';
-import { defineProps, ref } from 'vue';
+import { useStorage } from '@vueuse/core';
+import { defineProps } from 'vue';
 
 defineProps({
 	msg: {
