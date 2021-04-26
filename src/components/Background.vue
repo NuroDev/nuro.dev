58298eb64288c2feb30af97e135a34962175a3fc
nurodev <me@nuro.dev>
2021-04-27 00:06:27 +0100

    Added basic transition to OGL canvas

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,5 +1,14 @@
 <template>
-	<div ref="background" class="background" />
+	<transition
+		enter-active-class="transition duration-500 ease-out"
+		enter-from-class="transform opacity-0"
+		enter-to-class="transform opacity-100"
+		leave-active-class="transition duration-500 ease-in"
+		leave-from-class="transform opacity-100"
+		leave-to-class="transform opacity-0"
+	>
+		<div ref="background" class="background" />
+	</transition>
 </template>
 
 <script lang="ts" setup>
