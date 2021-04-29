fcd3e600797c3be4572971d1d8b4b496f00675bd
nurodev <me@nuro.dev>
2021-04-29 01:21:01 +0100

    Initial web vitals source commit

---
+++ src/vitals.ts
@@ -0,0 +1,80 @@
+import { getCLS, getFCP, getFID, getLCP, getTTFB } from 'web-vitals';
+
+import type { RouteLocationNormalized } from 'vue-router';
+import type { Metric } from 'web-vitals';
+
+interface IVitalsOptions {
+	debug?: true;
+	route: RouteLocationNormalized;
+}
+
+interface IContext {
+	fullPath: string;
+	href: string;
+}
+
+interface ISendOptions {
+	context: IContext;
+	debug: boolean;
+	metric: Metric;
+}
+
+const VITALS_URL = 'https://vitals.vercel-analytics.com/v1/vitals';
+
+const sendMetric = ({ context, debug, metric }: ISendOptions) => {
+	const speed: string =
+		'connection' in navigator &&
+		navigator['connection'] &&
+		'effectiveType' in navigator['connection']
+			? navigator['connection']['effectiveType']
+			: '';
+
+	const dsn: string = import.meta.env[
+		import.meta.env.DEV ? 'VITE_VERCEL_ANALYTICS_ID' : 'VERCEL_ANALYTICS_ID'
+	] as string;
+
+	const body = {
+		dsn,
+		event_name: metric.name,
+		href: context.href,
+		id: metric.id,
+		page: context.fullPath,
+		speed,
+		value: metric.value.toString(),
+	};
+
+	if (debug) console.debug(`${metric.name}:`, body);
+
+	// This content type is necessary for `sendBeacon`
+	const blob = new Blob([new URLSearchParams(body).toString()], {
+		type: 'application/x-www-form-urlencoded',
+	});
+
+	navigator.sendBeacon
+		? navigator.sendBeacon(VITALS_URL, blob)
+		: fetch(VITALS_URL, {
+				body: blob,
+				credentials: 'omit',
+				keepalive: true,
+				method: 'POST',
+		  });
+};
+
+export function useVitals(options: IVitalsOptions): void {
+	const { debug = import.meta.env.DEV, route } = options;
+
+	const context: IContext = {
+		fullPath: route.fullPath,
+		href: location.href,
+	};
+
+	try {
+		getFID((metric: Metric) => sendMetric({ context, metric, debug }));
+		getTTFB((metric: Metric) => sendMetric({ context, metric, debug }));
+		getLCP((metric: Metric) => sendMetric({ context, metric, debug }));
+		getCLS((metric: Metric) => sendMetric({ context, metric, debug }));
+		getFCP((metric: Metric) => sendMetric({ context, metric, debug }));
+	} catch (error) {
+		console.error('[Analytics]', error);
+	}
+}
