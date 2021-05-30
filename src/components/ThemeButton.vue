3dbc19b9fd4260ab9e5c8d9bac6a460498043399
nurodev <me@nuro.dev>
2021-05-30 23:54:31 +0100

    Temp disabled theme button press animation

--- src/components/ThemeButton.vue
+++ src/components/ThemeButton.vue
@@ -35,8 +35,8 @@ const toggleDark = useToggle(isDark);
 			text-gray-300 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-300 \
 			bg-gray-50 hover:bg-gray-100 hover:bg-opacity-50 dark:bg-gray-900 dark:hover:bg-gray-800 \
 			transition ease-in-out duration-300 \
-			focus:outline-none focus:ring-4 focus:ring-primary-500 \
-			transform translate-y-0 hover:-translate-y-1 active:translate-y-1;
+			focus:outline-none focus:ring-4 focus:ring-primary-500;
+		/* transform translate-y-0 hover:-translate-y-1 active:translate-y-1; */
 
 		svg {
 			@apply w-6 h-6;
