af61b94cd1f5a2e6dbd1371f912dad3d79d29673
nurodev <me@nuro.dev>
2021-05-31 04:53:32 +0100

    Added basic frontmatter example

--- src/components/blog/Post.vue
+++ src/components/blog/Post.vue
@@ -3,9 +3,14 @@
 </template>
 
 <script setup lang="ts">
+import { useHead } from '@vueuse/head';
 import { defineProps } from 'vue';
 
 const { frontmatter } = defineProps<{
 	frontmatter: any;
 }>();
+
+useHead({
+	title: `nuro ─ ${frontmatter.title}`,
+});
 </script>
--- src/content/blog/2021/hello_world.md
+++ src/content/blog/2021/hello_world.md
@@ -1,3 +1,7 @@
+---
+title: Hello World
+---
+
 # Hello World
 
 Foo bar
