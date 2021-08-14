6994c1dbdbf469a1938b870e9465c8abfc85410b
nurodev <me@nuro.dev>
2021-08-14 10:40:31 +0100

    Added Attio logo to business pill

--- src/components/BusinessPill.vue
+++ src/components/BusinessPill.vue
@@ -1,5 +1,6 @@
 <template>
 	<a class="pill" :href="url" target="_black" rel="noreferrer noopener">
+		<img :src="logo" alt="Attio Logo" :draggable="false" />
 		<span class="content">
 			<slot />
 		</span>
@@ -17,11 +18,16 @@ defineProps<{
 <style lang="postcss" scoped>
 .pill {
 	@apply inline-flex justify-center items-center \
-		mt-0 my-2 px-3 \
-		hover:bg-gray-500 hover:bg-opacity-15 \
+		px-2 pb-0.5 \
+		hover:bg-gray-300 dark:hover:bg-gray-500 hover:bg-opacity-15 \
 		hover:backdrop-filter hover:backdrop-blur-sm hover:saturate-200 \
-		hover:text-white \
 		rounded-lg cursor-pointer \
 		transition ease-in-out duration-300;
+
+	img {
+		@apply w-4 h-4 \
+			mr-1 \
+			select-none;
+	}
 }
 </style>
