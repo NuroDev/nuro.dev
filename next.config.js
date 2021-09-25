cf61aa3abbf1fa79a89a29ce577756e8aa61e64f
nurodev <me@nuro.dev>
2021-09-25 20:07:05 +0100

    Replace React with Preact in production

--- next.config.js
+++ next.config.js
@@ -35,6 +35,18 @@ module.exports = {
 		];
 	},
 	reactStrictMode: true,
+	webpack: (config, { dev, isServer }) => {
+		// Replace React with Preact only in client production build
+		if (!dev && !isServer) {
+			Object.assign(config.resolve.alias, {
+				react: 'preact/compat',
+				'react-dom/test-utils': 'preact/test-utils',
+				'react-dom': 'preact/compat',
+			});
+		}
+
+		return config;
+	},
 };
 
 const ContentSecurityPolicy = `
