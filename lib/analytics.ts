75673f49f4f09a41b43ecc52e7456a4c3df06b9e
nurodev <me@nuro.dev>
2021-09-25 20:48:18 +0100

    Disabled analytics during development

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -3,6 +3,8 @@ import { useEffect } from 'react';
 
 export function useAnalytics() {
 	useEffect(() => {
+		if (process.env.NODE_ENV !== 'production') return;
+
 		splitbee.init({
 			disableCookie: false,
 		});
