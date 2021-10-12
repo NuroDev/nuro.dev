4096071cd37d2dfcd484c57e73ab11219a147afa
nurodev <me@nuro.dev>
2021-10-12 16:20:55 +0100

    Fixed desktop navbar height

--- components/Navbar/Standard.component.tsx
+++ components/Navbar/Standard.component.tsx
@@ -26,7 +26,7 @@ const navigation: NavigationItems = [
 	},
 ];
 
-const StyledDisclosure = styled(Disclosure)(tw`fixed inset-0`);
+const StyledDisclosure = styled(Disclosure)(tw`fixed inset-0 h-24`);
 
 const Container = styled.div(tw`
 	max-w-4xl mx-auto px-2 sm:px-6 lg:px-8
