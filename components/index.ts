c8d32cb139aaa757fbcefb18d03cae3b04a29c6b
nurodev <me@nuro.dev>
2021-10-12 23:49:52 +0100

    Add Nprogress

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,7 @@
 export * as Background from './Background';
-export * from './Button';
 export * as Blog from './Blog';
 export * as Navbar from './Navbar';
+export * from './Button';
 export * from './Pill.component';
 
 export * from './Wave.styles';
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,46 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import tw, { GlobalStyles as TailwindStyles } from 'twin.macro';
 import { AppProps } from 'next/app';
 import { css, Global as EmotionStyles } from '@emotion/react';
 import { ThemeProvider } from 'next-themes';
 
 import 'inter-ui/inter.css';
+import 'nprogress/nprogress.css';
 
 import { useAnalytics } from '~/lib';
 import { Theme } from '~/types';
+
+NProgress.configure({
+	minimum: 0.3,
+	easing: 'ease',
+	speed: 800,
+	showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const GlobalStyles = css`
 	html {
 		${tw`
 			antialiased \
-			bg-gray-50 text-gray-500 dark:text-gray-400 \
+			bg-gray-50 \
+			text-gray-500 dark:text-gray-400 \
 			transition ease-in-out duration-300
 		`}
 		font-family: "Inter", "system-ui";
-	}
 
-	html.dark {
-		${tw`bg-gray-900`}
+		&.dark {
+			${tw`bg-gray-900`}
 
-		* {
-			--tw-ring-offset-color: #0c0e10;
+			* {
+				--tw-ring-offset-color: #0c0e10;
 
-			&::selection {
-				${tw`
-						bg-white \
-						text-primary-500
-					`}
+				&::selection {
+					${tw`bg-white text-primary-500`}
+				}
 			}
 		}
 	}
@@ -41,6 +53,10 @@ const GlobalStyles = css`
 		html {
 			font-family: 'Inter', 'system-ui';
 		}
+	}
+
+	#nprogress .bar {
+		${tw`h-1 bg-gray-800 dark:bg-white`}
 	}
 `;
 
