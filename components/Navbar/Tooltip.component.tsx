a2ae72e191ccb314b5cc44c438eb4ce357022836
nurodev <me@nuro.dev>
2021-10-12 13:54:15 +0100

    Added background blur to navbar item tooltip

--- components/Navbar/Tooltip.component.tsx
+++ components/Navbar/Tooltip.component.tsx
@@ -4,7 +4,7 @@ import tw from 'twin.macro';
 export const Container = styled.div(tw`
 	absolute top-full -left-1/2 z-10 \
 	mt-3 ml-2 px-4 py-2 \
-	bg-transparent \
+	bg-gray-100 bg-opacity-50 dark:bg-gray-900 dark:bg-opacity-50 backdrop-filter backdrop-blur-sm \
 	border border-gray-100 dark:border-gray-500 \
 	text-gray-400 dark:text-white text-center text-xs \
 	rounded-lg pointer-events-none \
