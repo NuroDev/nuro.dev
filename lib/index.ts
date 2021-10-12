cb43a54cad1720d928be1ed23ac31272d00974a5
nurodev <me@nuro.dev>
2021-10-12 18:27:42 +0100

    Updated lib file names

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,2 @@
-export * from './analytics';
-export * from './seo';
+export * from './useAnalytics';
+export * from './useSeo';
---
+++ lib/useAnalytics.ts
@@ -0,0 +1,12 @@
+import splitbee from '@splitbee/web';
+import { useEffect } from 'react';
+
+export function useAnalytics() {
+	useEffect(() => {
+		if (process.env.NODE_ENV !== 'production') return;
+
+		splitbee.init({
+			disableCookie: false,
+		});
+	}, []);
+}
