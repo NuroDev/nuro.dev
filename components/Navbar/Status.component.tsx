6c7dde73bf76ca13306fd25a0b5dd8d6816cf346
nurodev <me@nuro.dev>
2021-10-13 19:22:09 +0100

    Switched to using react-use-lanyard

--- components/Navbar/Status.component.tsx
+++ components/Navbar/Status.component.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 import styled from '@emotion/styled';
 import tw from 'twin.macro';
-import { useLanyard } from 'use-lanyard';
+import { useLanyard } from 'react-use-lanyard';
 
 import { Status } from '..';
 import { DiscordStatus } from '~/types';
 
-import type { Data } from 'use-lanyard';
+import type { LanyardData } from 'react-use-lanyard';
 
 const Container = styled.div(tw`
 	ml-3 mt-3 \
@@ -15,11 +15,14 @@ const Container = styled.div(tw`
 
 export function StatusWrapper() {
 	const userId = process.env.NEXT_PUBLIC_DISCORD_ID;
-	const { data: status } = useLanyard(userId);
+	const { loading, status } = useLanyard({
+		userId,
+		socket: true,
+	});
 
-	if (!userId || !status || status.discord_status === DiscordStatus.OFFLINE) return null;
+	if (!userId || loading || status.discord_status === DiscordStatus.OFFLINE) return null;
 
-	function statusColor(status: Data) {
+	function statusColor(status: LanyardData) {
 		switch (status.discord_status as DiscordStatus) {
 			case DiscordStatus.ONLINE:
 				return 'green';
