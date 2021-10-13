f0f64178f05a8c19fb7457127ae1010f47e06714
nurodev <me@nuro.dev>
2021-10-13 09:41:52 +0100

    Added optional background prop to default layout

--- layouts/Default.layout.tsx
+++ layouts/Default.layout.tsx
@@ -8,21 +8,21 @@ import { Background, Navbar } from '~/components';
 import type { WithChildren, WithProps } from '~/types';
 
 interface DefaultLayoutProps extends WithChildren {
+	background?: boolean;
 	seo?: Partial<WithProps<typeof NextSeo>>;
 }
 
 const Main = styled.main(tw`flex flex-col justify-center px-1 sm:px-8`);
 
-export function DefaultLayout({ children, seo }: DefaultLayoutProps) {
+export function DefaultLayout({ background = true, children, seo }: DefaultLayoutProps) {
 	const defaultSeoProps = useSeoProps();
 
 	return (
 		<>
 			<NextSeo {...defaultSeoProps} {...seo} />
 			<Navbar.Standard />
-
 			<Main>
-				<Background.Standard />
+				{background && <Background.Standard />}
 				{children}
 			</Main>
 		</>
