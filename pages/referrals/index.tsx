8e6deaf282ae93066427bf59324f5bafb1eed2e5
nurodev <me@nuro.dev>
2021-10-15 15:24:45 +0100

    Referall card style tweaks

--- pages/referrals/index.tsx
+++ pages/referrals/index.tsx
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic';
 import styled from '@emotion/styled';
 import tw from 'twin.macro';
 import { Icon } from '@iconify/react';
@@ -5,7 +6,6 @@ import { useCopyToClipboard } from 'react-use';
 
 import { Button, Pill } from '~/components';
 import { Layout } from '~/layouts';
-import { useClick } from '~/lib';
 
 import type { GetStaticProps } from 'next';
 
@@ -23,15 +23,21 @@ const Container = styled.div(tw`
 	flex items-center justify-center w-full h-screen
 `);
 
-const CardContainer = styled.div(tw`
-	flex items-center space-x-2 \
-	m-2 p-4 \
-	rounded-lg
-	border border-gray-200 dark:border-gray-500 \
-	transition ease-in-out duration-300
+const Content = styled.div(tw`
+	max-w-3xl overflow-hidden
 `);
 
-const Spacer = styled.span(tw`flex-1`);
+const List = styled.ul(tw`
+	flex flex-col space-y-4
+`);
+
+const ListItem = styled.li(tw`
+	bg-gray-50 bg-opacity-75 dark:bg-gray-900 dark:bg-opacity-75 \
+	backdrop-filter backdrop-blur-sm \
+	border border-gray-100 dark:border-gray-500 \
+	rounded-lg \
+	transition ease-in-out duration-300
+`);
 
 export const getStaticProps: GetStaticProps<ReferralsProps> = async () => {
 	const { default: rawReferrals } = await import('~/data/referrals.json');
@@ -53,34 +59,53 @@ export const getStaticProps: GetStaticProps<ReferralsProps> = async () => {
 };
 
 function ReferralCard({ referral }: ReferralCardProps) {
-	const [click] = useClick();
 	const [state, copyToClipboard] = useCopyToClipboard();
 
 	return (
-		<CardContainer>
-			<h1>{referral.name}</h1>
-			<Icon icon={`feather:${referral.icon}`} />
-			<Spacer />
-			{referral.bonus && <Pill>{referral.bonus}</Pill>}
-			{referral.code && (
-				<Button.Icon
-					tw="w-10 h-10"
-					type="button"
-					onClick={() => copyToClipboard(referral.code)}>
-					<Icon icon={`feather:hash`} />
-				</Button.Icon>
-			)}
-			<Button.Icon tw="w-10 h-10" href={referral.url} onClick={() => click}>
-				<Icon icon={`feather:link`} />
-			</Button.Icon>
-
-			{/* TODO: Use `react-hot-toast instead */}
-			{/* {state.error
-				? <p>Unable to copy value: {state.error.message}</p>
-				: state.value && <p>Copied {state.value}</p>} */}
-
-			{/* {JSON.stringify(referral, null, 4)} */}
-		</CardContainer>
+		<ListItem>
+			<div tw="flex items-center px-4 py-4 sm:px-6">
+				<div tw="min-w-0 flex flex-1 items-center">
+					<div tw="flex-shrink-0">
+						<Icon icon={`feather:${referral.icon}`} tw="w-6 h-6 dark:text-gray-400" />
+					</div>
+					<div tw="min-w-0 flex-1 px-4">
+						<div>
+							<p tw="text-lg font-bold text-gray-700 dark:text-white truncate">
+								{referral.name}
+							</p>
+							<p tw="mt-1 flex items-center text-xs text-gray-500 dark:text-gray-400">
+								<span>{referral.description}</span>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div tw="flex items-center space-x-2">
+					<Button.Icon
+						tw="w-10 h-10 border border-gray-100 dark:border-gray-500"
+						href={referral.url}
+						// onClick={() => click}
+					>
+						<Icon icon="feather:home" />
+					</Button.Icon>
+					{referral.code && (
+						<Button.Icon
+							tw="w-10 h-10 border border-gray-100 dark:border-gray-500"
+							type="button"
+							// onClick={() => copyToClipboard(referral.code)}
+						>
+							<Icon icon="feather:hash" />
+						</Button.Icon>
+					)}
+					<Button.Icon
+						tw="w-10 h-10 border border-gray-100 dark:border-gray-500"
+						href={referral.url}
+						// onClick={() => click}
+					>
+						<Icon icon="feather:external-link" />
+					</Button.Icon>
+				</div>
+			</div>
+		</ListItem>
 	);
 }
 
@@ -94,58 +119,13 @@ export default function ReferralsPage({ referrals }: ReferralsProps) {
 			</Container> */}
 
 			<Container>
-				<div tw="max-w-3xl overflow-hidden">
-					<ul role="list" tw="flex flex-col space-y-4">
+				<Content>
+					<List role="list">
 						{referrals.map((referral) => (
-							<li
-								key={referral.icon}
-								tw="bg-gray-50 bg-opacity-75 dark:bg-gray-900 dark:bg-opacity-75 backdrop-filter backdrop-blur-sm border border-gray-100 dark:border-gray-500 rounded-lg transition ease-in-out duration-300">
-								<div tw="flex items-center px-4 py-4 sm:px-6">
-									<div tw="min-w-0 flex-1 flex items-center">
-										<div tw="flex-shrink-0">
-											<Icon icon={`feather:feather`} tw="w-10 h-10" />
-										</div>
-										<div tw="min-w-0 flex-1 px-4">
-											<div>
-												<p tw="text-lg font-bold text-gray-700 dark:text-white truncate">
-													{referral.name}
-												</p>
-												<p tw="mt-1 flex items-center text-xs text-gray-500 dark:text-gray-400">
-													<span>{referral.description}</span>
-												</p>
-											</div>
-										</div>
-									</div>
-									<div tw="flex items-center space-x-2">
-										<Button.Icon
-											tw="w-10 h-10 border border-gray-100 dark:border-gray-500"
-											href={referral.url}
-											// onClick={() => click}
-										>
-											<Icon icon="feather:home" />
-										</Button.Icon>
-										{referral.code && (
-											<Button.Icon
-												tw="w-10 h-10 border border-gray-100 dark:border-gray-500"
-												type="button"
-												// onClick={() => copyToClipboard(referral.code)}
-											>
-												<Icon icon="feather:hash" />
-											</Button.Icon>
-										)}
-										<Button.Icon
-											tw="w-10 h-10 border border-gray-100 dark:border-gray-500"
-											href={referral.url}
-											// onClick={() => click}
-										>
-											<Icon icon="feather:external-link" />
-										</Button.Icon>
-									</div>
-								</div>
-							</li>
+							<ReferralCard key={referral.name} referral={referral} />
 						))}
-					</ul>
-				</div>
+					</List>
+				</Content>
 			</Container>
 		</Layout.Default>
 	);
