5577dcc00e088e06d683e47991d337af37b82064
nurodev <me@nuro.dev>
2021-10-19 13:14:10 +0100

    Lowered click sound volume

--- lib/sounds.ts
+++ lib/sounds.ts
@@ -4,7 +4,7 @@ import { usePersistantState } from '.';
 export function useClick() {
 	const state = usePersistantState();
 	const result = useSound('/sounds/click.ogg', {
-		volume: 0.25,
+		volume: 0.05,
 	});
 
 	if (!state.get().sound) return [() => {}, null];
