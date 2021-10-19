b30a8cb231893a0e8af0546e811b9304d9c542de
nurodev <me@nuro.dev>
2021-10-19 13:46:36 +0100

    Added back description with typed animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import tw from 'twin.macro';
 import Typed from 'typed.js';
+import { differenceInYears } from 'date-fns';
 import { Icon } from '@iconify/react';
 import { Transition } from '@headlessui/react';
 import { useEffect, useRef } from 'react';
@@ -27,6 +28,12 @@ const Title = styled.h1(tw`
 	tracking-tight font-extrabold
 `);
 
+const Description = styled.p(tw`
+	max-w-xs \
+	mt-4 md:mt-8 mx-auto \
+	text-base text-gray-300 sm:text-lg md:text-xl md:max-w-3xl
+`);
+
 const LineBreak = styled.br(tw`
 	hidden sm:block
 `);
@@ -39,7 +46,7 @@ const ActionsTransition = styled(Transition)<{ $delayStagger: number }>`
 	&.enter {
 		${tw`transition ease-in-out duration-500`}
 
-		transition-delay: ${({ $delayStagger }) => 2200 + $delayStagger}ms;
+		transition-delay: ${({ $delayStagger }) => 3500 + $delayStagger}ms;
 	}
 	&.enterFrom {
 		${tw`transform scale-95 opacity-0`}
@@ -96,21 +103,36 @@ const ACTIONS: Array<NavigationItem> = [
 export default function HomePage() {
 	const titleRef = useRef(null);
 	const titleElementsRef = useRef(null);
-	const typed = useRef(null);
+	const titleTyped = useRef(null);
+
+	const descriptionRef = useRef(null);
+	const descriptionTyped = useRef(null);
 
 	const today = new Date();
 	const birthday = new Date('1997-08-09');
+	const age = differenceInYears(today, birthday);
+
 	const isBirthday =
 		today.getDate() === birthday.getDate() && today.getMonth() === birthday.getMonth();
 
 	useEffect(() => {
-		typed.current = new Typed(titleRef.current, {
+		titleTyped.current = new Typed(titleRef.current, {
 			showCursor: false,
 			stringsElement: titleElementsRef.current,
-			typeSpeed: 50,
+			typeSpeed: 40,
 		});
 
-		return () => typed.current.destroy();
+		descriptionTyped.current = new Typed(descriptionRef.current, {
+			showCursor: false,
+			startDelay: 2000,
+			strings: [`I am a ${age} year old software engineer and games developer`],
+			typeSpeed: 15,
+		});
+
+		return () => {
+			titleTyped.current.destroy();
+			descriptionTyped.current.destroy();
+		};
 	}, []);
 
 	return (
@@ -127,6 +149,7 @@ export default function HomePage() {
 						</span>
 						<Title ref={titleRef} />
 					</>
+					<Description ref={descriptionRef}></Description>
 					<Actions>
 						{ACTIONS.map((action, index) => {
 							if (action.type !== NavigationItemType.LINK) return null;
