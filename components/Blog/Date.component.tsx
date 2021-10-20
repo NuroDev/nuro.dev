cce247b585f15cbfb81c20f8b3d603d33933ca60
nurodev <me@nuro.dev>
2021-10-20 15:24:10 +0100

    Added "small" prop to blog date pill component

--- components/Blog/Date.component.tsx
+++ components/Blog/Date.component.tsx
@@ -4,23 +4,28 @@ import { Icon } from '@iconify/react';
 
 import type { WithChildren, WithClassName } from '~/types';
 
-interface DateProps extends WithClassName, WithChildren {}
+interface DateProps extends WithClassName, WithChildren {
+	small?: boolean;
+}
 
-const Container = styled.div(tw`
-	inline-flex \
-	px-4 py-2 \
-	bg-primary-500 bg-opacity-15 \
-	rounded-lg \
-	text-sm text-primary-800
-`);
+const Container = styled.div<{ small: boolean }>`
+	${tw`
+		inline-flex \
+		bg-primary-500 bg-opacity-15 \
+		rounded-lg \
+		text-sm text-primary-800
+	`}
+
+	${({ small }) => (small ? tw`px-2 py-1` : tw`px-4 py-2`)}
+`;
 
 const StyledIcon = styled(Icon)(tw`
 	mt-0.5 mr-3
 `);
 
-export function Date({ children, className }: DateProps) {
+export function Date({ children, className, small = false }: DateProps) {
 	return (
-		<Container className={className}>
+		<Container className={className} small={small}>
 			<StyledIcon icon="feather:calendar" />
 			{children}
 		</Container>
