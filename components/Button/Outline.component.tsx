f63d1d1f20074c6318f7b730d7474df78e6cbc69
nurodev <me@nuro.dev>
2021-10-20 15:50:22 +0100

    Updated outline button styles

--- components/Button/Outline.component.tsx
+++ components/Button/Outline.component.tsx
@@ -12,19 +12,23 @@ import type { WithClassName } from '~/types';
 interface OutlineProps extends AnchorHTMLAttributes<HTMLAnchorElement>, WithClassName {
 	external?: boolean;
 	icon?: string;
+	small?: boolean;
 }
 
-const Container = styled.a(tw`
-	inline-flex items-center justify-center w-full sm:w-auto \
-	px-8 py-2 \
-	bg-gray-50 bg-opacity-75 hover:bg-gray-100 hover:bg-opacity-75 dark:bg-gray-900 dark:bg-opacity-75 dark:hover:bg-gray-800 dark:hover:bg-opacity-75 \
-	backdrop-filter backdrop-blur-sm saturate-200 \
-	text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 \
-	border-2 border-primary-100 dark:border-gray-700 \
-	rounded-lg cursor-pointer \
-	transition ease-in-out duration-300 \
-	focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-primary-500
-`);
+const Container = styled.a<{ small?: boolean }>`
+	${tw`
+		inline-flex items-center justify-center w-full sm:w-auto \
+		bg-gray-50 bg-opacity-75 hover:bg-gray-100 hover:bg-opacity-75 dark:bg-gray-900 dark:bg-opacity-75 dark:hover:bg-gray-800 dark:hover:bg-opacity-75 \
+		backdrop-filter backdrop-blur-sm saturate-200 \
+		text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 \
+		border-2 border-gray-200 dark:border-gray-700 \
+		rounded-lg cursor-pointer \
+		transition ease-in-out duration-300 \
+		focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-primary-500
+	`}
+
+	${({ small }) => (small ? tw`px-4 py-1 text-sm` : tw`px-8 py-2`)}
+`;
 
 const StyledIcon = styled(Icon)(tw`mt-1 mr-3`);
 
@@ -35,6 +39,7 @@ export function Outline({
 	href,
 	icon,
 	onClick,
+	small = false,
 	...rest
 }: OutlineProps) {
 	const [play] = useClick();
@@ -49,6 +54,7 @@ export function Outline({
 					if (onClick) onClick(...args);
 				}}
 				rel="noopener noreferrer"
+				small={small}
 				target={external ? '_blank' : undefined}
 				{...rest}>
 				{icon && <StyledIcon icon={icon} />}
