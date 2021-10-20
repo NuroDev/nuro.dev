33628e30b1e110db2fc464bf298bc876e6710523
nurodev <me@nuro.dev>
2021-10-20 13:14:33 +0100

    Added Discord assets to image config

--- next.config.js
+++ next.config.js
@@ -5,12 +5,15 @@ module.exports = {
 	},
 	images: {
 		domains: [
-			// Unsplash
-			'source.unsplash.com',
-			'images.unsplash.com',
+			// Discord assets
+			'cdn.discordapp.com',
 
 			// Spotify Album Art
 			'i.scdn.co',
+
+			// Unsplash
+			'source.unsplash.com',
+			'images.unsplash.com',
 		]
 	},
 	// Inspired by: https://github.com/leerob/leerob.io/blob/main/next.config.js#L44-L81
