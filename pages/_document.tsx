fb21343f95751409eb9708803fb2f5a2f265cf0c
nurodev <me@nuro.dev>
2021-11-19 00:40:30 +0000

    Updated favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<link rel="icon" type="image/png" href="/favicon.png" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
