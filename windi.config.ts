0047a7b398f16f1cb4062587beadb815963e091f
nurodev <me@nuro.dev>
2022-05-09 23:44:36 +0100

    Added focus/transition shortcuts to WindiCSS config

--- windi.config.ts
+++ windi.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
 		exclude: ['.git', '.next', 'node_modules'],
 	},
 	plugins: [require('windicss/plugin/line-clamp'), require('windicss/plugin/typography')],
+	shortcuts: {
+		'default-focus':
+			'focus:(outline-none ring-4 ring-offset-4 dark:ring-offset-gray-900 ring-primary-500)',
+		'default-transition': 'transition ease-in-out duration-300',
+	},
 	theme: {
 		extend: {
 			colors,
