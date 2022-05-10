137a52c90b49e29722d89b02a61da0e5522daee9
nurodev <me@nuro.dev>
2022-05-10 11:08:11 +0100

    Improved grid initial layout design

--- pages/grid/index.tsx
+++ pages/grid/index.tsx
@@ -1,6 +1,13 @@
 import clsx from 'clsx';
+import { useTheme } from 'next-themes';
+import { useCallback, useState } from 'react';
+
+import { Animate } from '~/components';
 import { Layout } from '~/layouts';
-import { WithChildren, WithClassName } from '~/types';
+import { Theme } from '~/types';
+
+import type { WithChildren, WithClassName } from '~/types';
+import { Icon } from '@iconify/react';
 
 interface CardProps extends WithChildren, WithClassName {}
 
@@ -16,16 +23,58 @@ function Card({ children, className }: CardProps) {
 	);
 }
 
+const cards = [
+	null,
+	{ className: 'md:col-span-2' },
+	null,
+	{ className: 'md:col-span-2' },
+	null,
+	null,
+];
+
+const THEME_ICON: Record<Theme, string> = {
+	[Theme.SYSTEM]: 'feather:monitor',
+	[Theme.LIGHT]: 'feather:sun',
+	[Theme.DARK]: 'feather:moon',
+};
+
 export default function GridIndexPage() {
+	const { setTheme, theme } = useTheme();
+
+	const [themeCycle, setThemeCycle] = useState<number>(0);
+
+	const handleThemeChange = useCallback(() => {
+		let nextThemeCycle = themeCycle + 1;
+		if (nextThemeCycle >= Object.keys(Theme).length) nextThemeCycle = 0;
+		setThemeCycle(nextThemeCycle);
+
+		setTheme(Object.values(Theme)[nextThemeCycle]);
+	}, [setTheme, themeCycle]);
+
 	return (
 		<Layout.Base className="min-h-screen p-8 overflow-hidden">
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-				<Card>1</Card>
-				<Card className="md:col-span-2">2</Card>
-				<Card>3</Card>
-				<Card className="md:col-span-2">4</Card>
-				<Card>5</Card>
-				<Card>6</Card>
+				{cards.map((card, i) => (
+					<Animate
+						as="button"
+						animation={{ y: [50, 0], opacity: [0, 1] }}
+						className={clsx(card?.className, 'bg-transparent cursor-pointer')}
+						key={i}
+						transition={{ delay: 0.1 * (i + 1), duration: 0.5, repeat: 0 }}
+						onClick={handleThemeChange}>
+						<Card className="flex-col space-y-2">
+							<div className="space-x-4">
+								<Icon icon={THEME_ICON[theme]} />
+								<span>
+									{theme?.charAt(0).toUpperCase() + theme?.slice(1)} Theme
+								</span>
+							</div>
+							<p className="text-xs text-gray-400 tracking-wide">
+								Click to cycle through
+							</p>
+						</Card>
+					</Animate>
+				))}
 			</div>
 		</Layout.Base>
 	);
