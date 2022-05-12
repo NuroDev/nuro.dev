f5150e731b58a22cbdfad6faba909bef09306102
nurodev <me@nuro.dev>
2022-05-12 17:59:45 +0100

    Updated `Animate` component

--- components/Animate.component.tsx
+++ components/Animate.component.tsx
@@ -1,44 +1,52 @@
-import { animate } from 'motion';
+import { animate, spring } from 'motion';
+import { isCrawlerUserAgent } from 'is-web-crawler';
 import { useEffect, useRef } from 'react';
 import { useMedia } from 'react-use';
-import { spring } from 'motion';
 
 import type { AnimationOptionsWithOverrides, MotionKeyframesDefinition } from '@motionone/dom';
-import type { HTMLAttributes } from 'react';
+import type { ComponentPropsWithRef, ElementType } from 'react';
 
-interface AnimateProps extends HTMLAttributes<HTMLElement> {
+type AnimateProps<T extends ElementType> = {
 	animation: MotionKeyframesDefinition;
-	as?: React.ElementType;
+	as?: T;
+	enabled?: boolean;
 	transition?: AnimationOptionsWithOverrides;
-}
+} & Omit<ComponentPropsWithRef<T>, 'animation' | 'as' | 'transition'>;
 
-export function Animate({
+const defaultTransition: AnimationOptionsWithOverrides = {
+	delay: 0,
+	duration: 1500,
+	easing: spring(),
+	repeat: 0,
+};
+
+/**
+ * Animate
+ *
+ * @description A small wrapper component for the `motion` library to make it easier to use with React.js
+ */
+export function Animate<T extends ElementType>({
 	animation,
-	as: Component = 'div',
+	as: Component = 'div' as T,
 	children,
+	enabled = true,
 	transition,
 	...rest
-}: AnimateProps): JSX.Element {
+}: AnimateProps<T>): JSX.Element {
 	const prefersReducedMotion = useMedia('(prefers-reduced-motion)', true);
 
 	const ref = useRef<HTMLElement | null>(null);
 
 	useEffect(() => {
-		if (ref.current && !prefersReducedMotion)
-			animate(
-				ref.current,
-				animation,
-				Object.assign(
-					{
-						easing: spring(),
-						repeat: 0,
-					} as AnimationOptionsWithOverrides,
-					transition,
-				),
-			);
-	}, [animation, children, prefersReducedMotion, ref, transition]);
+		if (ref.current && enabled && !(prefersReducedMotion || isCrawlerUserAgent()))
+			animate(ref.current, animation, {
+				...defaultTransition,
+				...transition,
+			});
+	}, [animation, enabled, prefersReducedMotion, transition]);
 
 	return (
+		// @ts-expect-error
 		<Component ref={ref} {...rest}>
 			{children}
 		</Component>
