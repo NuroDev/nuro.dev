cacc8676521af5901ef599874e4c9fd9094bead0
nurodev <me@nuro.dev>
2022-05-12 18:58:27 +0100

    Improved settings dropdown theme options

--- lib/navigation.tsx
+++ lib/navigation.tsx
@@ -4,7 +4,7 @@ import { useTheme } from 'next-themes';
 import { Status } from '~/components';
 import { usePersistantState, useStatus } from '~/lib';
 
-import { DiscordStatus, NavigationItemType, Theme } from '~/types';
+import { NavigationItemType, Theme } from '~/types';
 
 import type { NavigationItem, NavigationItems } from '~/types';
 
@@ -98,7 +98,7 @@ export function useNavigation() {
 			{
 				type: NavigationItemType.ACTION,
 				icon: 'feather:image',
-				endIcon: background ? 'feather:check-square' : 'feather:square',
+				endIcon: background ? 'feather:check-circle' : 'feather:square',
 				text: `Animations ${background ? 'On' : 'Off'}`,
 				onClick: () =>
 					state.set((settings) => ({
@@ -108,21 +108,38 @@ export function useNavigation() {
 			},
 			{
 				type: NavigationItemType.ACTION,
-				icon: 'feather:moon',
-				endIcon: isDark ? 'feather:check-square' : 'feather:square',
-				text: `Dark Theme ${isDark ? 'On' : 'Off'}`,
-				onClick: () => setTheme(isDark ? 'light' : 'dark'),
-			},
-			{
-				type: NavigationItemType.ACTION,
 				icon: sound ? 'feather:volume-2' : 'feather:volume-x',
-				endIcon: sound ? 'feather:check-square' : 'feather:square',
+				endIcon: sound ? 'feather:check-circle' : 'feather:square',
 				text: `Sounds ${sound ? 'On' : 'Off'}`,
 				onClick: () =>
 					state.set((settings) => ({
 						...settings,
 						sound: !settings.sound,
 					})),
+			},
+			{
+				type: NavigationItemType.DIVIDER,
+			},
+			{
+				type: NavigationItemType.ACTION,
+				icon: 'feather:monitor',
+				endIcon: theme === Theme.SYSTEM ? 'feather:check-circle' : undefined,
+				text: 'System Theme',
+				onClick: () => setTheme(Theme.SYSTEM),
+			},
+			{
+				type: NavigationItemType.ACTION,
+				icon: 'feather:sun',
+				endIcon: theme === Theme.LIGHT ? 'feather:check-circle' : undefined,
+				text: 'Light Theme',
+				onClick: () => setTheme(Theme.LIGHT),
+			},
+			{
+				type: NavigationItemType.ACTION,
+				icon: 'feather:moon',
+				endIcon: theme === Theme.DARK ? 'feather:check-circle' : undefined,
+				text: 'Dark Theme',
+				onClick: () => setTheme(Theme.DARK),
 			},
 		],
 	];
