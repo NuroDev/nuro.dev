ce472b7205be2733d22a0b391aa711032fa7b53b
nurodev <me@nuro.dev>
2022-05-13 17:15:40 +0100

    Moved WindiCSS colors to `lib` module

--- windi.config.ts
+++ windi.config.ts
@@ -1,6 +1,8 @@
 import defaultTheme from 'windicss/defaultTheme';
 import { defineConfig } from 'windicss/helpers';
 
+import { colors } from './lib/colors';
+
 export default defineConfig({
 	darkMode: 'class',
 	extract: {
@@ -18,32 +20,7 @@ export default defineConfig({
 			backgroundOpacity: {
 				15: '0.15',
 			},
-			colors: {
-				gray: {
-					50: '#f9fafb',
-					100: '#eaeaeb',
-					200: '#cacbcd',
-					300: '#a7a9ac',
-					400: '#696c71',
-					500: '#282d34',
-					600: '#24292f',
-					700: '#181b20',
-					800: '#121518',
-					900: '#0c0e10',
-				},
-				primary: {
-					50: '#32a4ff',
-					100: '#289aff',
-					200: '#1e90ff',
-					300: '#1486ff',
-					400: '#0a7cff',
-					500: '#0072ff',
-					600: '#0068f5',
-					700: '#005eeb',
-					800: '#0054e1',
-					900: '#004ad7',
-				},
-			},
+			colors,
 			fontFamily: {
 				sans: ['Inter', ...defaultTheme.fontFamily.sans],
 			},
