6d21282d8bfb9789d7e884f5687fc000f587d5ca
nurodev <me@nuro.dev>
2022-05-17 02:18:03 +0100

    Added ElementType prop to base layout

--- layouts/Base.layout.tsx
+++ layouts/Base.layout.tsx
@@ -1,20 +1,22 @@
 import { NextSeo } from 'next-seo';
+import { ElementType } from 'react';
 
 import { useSeoProps } from '~/lib/seo';
 
 import type { WithChildren, WithClassName, WithProps } from '~/types';
 
 interface BaseLayoutProps extends WithChildren, WithClassName {
+	as?: ElementType;
 	seo?: Partial<WithProps<typeof NextSeo>>;
 }
 
-export function BaseLayout({ children, className, seo }: BaseLayoutProps) {
+export function BaseLayout({ as: Main = 'main', children, className, seo }: BaseLayoutProps) {
 	const seoProps = useSeoProps(seo);
 
 	return (
 		<>
 			<NextSeo {...seoProps} />
-			<main className={className}>{children}</main>
+			<Main className={className}>{children}</Main>
 		</>
 	);
 }
