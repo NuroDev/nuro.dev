e1d3618407c18d95e27da35d856534cd4aa0e468
nurodev <me@nuro.dev>
2022-05-20 01:48:52 +0100

    Converted Status components to pure WindiCSS

--- components/Status/Widget/Loading.component.tsx
+++ components/Status/Widget/Loading.component.tsx
@@ -1,5 +1,4 @@
-import styled from '@emotion/styled';
-import tw from 'twin.macro';
+import clsx from 'clsx';
 
 import { LanyardAvatarType } from '~/types';
 
@@ -7,45 +6,19 @@ interface LoadingTypes {
 	type?: LanyardAvatarType;
 }
 
-const Container = styled.div(tw`
-	flex space-x-4 w-full max-w-sm \
-	mx-auto px-4 py-4 \
-	bg-white bg-opacity-50 dark:bg-gray-900 dark:bg-opacity-50 \
-	backdrop-filter backdrop-blur-sm \
-	border-2 border-gray-200 dark:border-gray-600 \
-	rounded-lg hover:shadow-lg \
-	transition ease-in-out duration-300 \
-	motion-safe:animate-pulse
-`);
-
-const Avatar = styled.div<{ $rounded: boolean }>`
-	${tw`
-		w-12 h-12 \
-		my-auto \
-		bg-gray-200 dark:bg-gray-600
-	`}
-
-	${({ $rounded }) => ($rounded ? tw`rounded` : tw`rounded-full`)}
-`;
-
-const LinesContainer = styled.div(tw`
-	flex-1 space-y-4 py-1
-`);
-
-const Line = styled.div(tw`
-	h-4 \
-	rounded \
-	bg-gray-200 dark:bg-gray-600
-`);
-
 export function Loading({ type }: LoadingTypes) {
 	return (
-		<Container>
-			<Avatar $rounded={type === LanyardAvatarType.MUSIC} />
-			<LinesContainer>
-				<Line tw="w-3/4" />
-				<Line />
-			</LinesContainer>
-		</Container>
+		<div className="flex space-x-4 w-full max-w-sm mx-auto px-4 py-4 bg-white bg-opacity-50 dark:(bg-gray-900 bg-opacity-50 border-gray-600) backdrop-filter backdrop-blur-sm border-2 border-gray-200 rounded-lg hover:shadow-lg default-transition motion-safe:animate-pulse">
+			<div
+				className={clsx(
+					'w-12 h-12 my-auto bg-gray-200 dark:bg-gray-600',
+					type === LanyardAvatarType.MUSIC ? 'rounded' : 'rounded-full',
+				)}
+			/>
+			<div className="flex-1 space-y-4 py-1">
+				<div className="w-3/4 h-4 rounded bg-gray-200 dark:bg-gray-600" />
+				<div className="h-4 rounded bg-gray-200 dark:bg-gray-600" />
+			</div>
+		</div>
 	);
 }
