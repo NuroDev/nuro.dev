dc693e92f1b28ba6244c2539bfe4240795a08730
nurodev <me@nuro.dev>
2022-05-21 16:49:21 +0100

    Fixed Icon button focus styles

--- components/Button/Icon.component.tsx
+++ components/Button/Icon.component.tsx
@@ -14,12 +14,11 @@ export const Icon = forwardRef<HTMLButtonElement, IconProps>(function Icon(
 			ref={ref}
 			className={clsx(
 				'group',
-				'relative inline-block px-3 py-2 bg-gray-50 hover:(bg-gray-100 text-gray-700) dark:(bg-gray-900 hover:bg-gray-700 hover:text-white) text-gray-400 rounded-lg text-sm font-medium default-transition focus:(outline-none ring-2 ring-offset-1 ring-primary-500)',
+				'relative inline-block px-3 py-2 bg-gray-50 hover:(bg-gray-100 text-gray-700) dark:(bg-gray-900 hover:bg-gray-700 hover:text-white) text-gray-400 rounded-lg text-sm font-medium default-transition default-focus',
 				className,
 			)}
 			onClick={(e) => onClick && onClick(e)}
-			{...rest}
-		>
+			{...rest}>
 			{children}
 		</button>
 	);
