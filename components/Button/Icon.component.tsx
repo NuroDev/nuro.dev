f9e416516362d01c8f1a73d6a821dd49a835f4df
nurodev <me@nuro.dev>
2022-05-21 19:53:15 +0100

    Fixed navbar button item position

--- components/Button/Icon.component.tsx
+++ components/Button/Icon.component.tsx
@@ -14,7 +14,7 @@ export const Icon = forwardRef<HTMLButtonElement, IconProps>(function Icon(
 			ref={ref}
 			className={clsx(
 				'group',
-				'relative inline-block px-3 py-2 bg-gray-50 hover:(bg-gray-100 text-gray-700) dark:(bg-gray-900 hover:bg-gray-700 hover:text-white) text-gray-400 rounded-lg text-sm font-medium default-transition default-focus',
+				'relative inline-flex items-center px-3 py-2 bg-gray-50 hover:(bg-gray-100 text-gray-700) dark:(bg-gray-900 hover:bg-gray-700 hover:text-white) text-gray-400 rounded-lg text-sm font-medium default-transition default-focus',
 				className,
 			)}
 			onClick={(e) => onClick && onClick(e)}
