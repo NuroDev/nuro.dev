c5338df79f495d51c624e6c0c36a42f41169b638
nurodev <me@nuro.dev>
2022-05-21 17:19:10 +0100

    Refactored error, latest & post blog components to pure WindiCSS

--- components/Blog/Error.component.tsx
+++ components/Blog/Error.component.tsx
@@ -1,5 +1,3 @@
-import styled from '@emotion/styled';
-import tw from 'twin.macro';
 import { Icon } from '@iconify/react';
 import { Layout } from '~/layouts';
 import { Button } from '..';
@@ -9,79 +7,49 @@ interface ErrorProps {
 	routeBlog?: boolean;
 }
 
-const Container = styled.div(tw`
-	flex flex-grow min-h-full \
-	pt-16 pb-12
-`);
-
-const Content = styled.div(tw`
-	flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto \
-	px-4 sm:px-6 lg:px-8
-`);
-
-const IconContainer = styled.div(tw`
-	flex-shrink-0 flex justify-center
-`);
-
-const StyledIcon = styled(Icon)(tw`
-	h-12 text-primary-500 w-auto
-`);
-
-const Body = styled.div(tw`py-4 text-center`);
-
-const Title = styled.h1(tw`
-	mt-2 \
-	text-4xl font-extrabold text-gray-500 dark:text-white tracking-tight sm:text-5xl
-`);
-
-const SubTitle = styled.p(tw`
-mt-4 \
-text-sm font-medium text-gray-300 dark:text-gray-400
-`);
-
-const Actions = styled.div(tw`
-	mt-6 flex justify-center items-center space-x-4
-`);
-
 export function Error({ routeBlog = true }: ErrorProps) {
 	return (
 		<Layout.Error>
-			<Container>
-				<Content>
-					<IconContainer>
-						<StyledIcon icon="feather:alert-circle" />
-					</IconContainer>
-					<Body>
-						<Title>No Posts Found</Title>
-						<SubTitle>Sorry, we couldn’t find any blog posts ¯\_(ツ)_/¯</SubTitle>
-						<Actions>
+			<div className="flex flex-grow min-h-full pt-16 pb-12">
+				<div className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+					<div className="flex-shrink-0 flex justify-center">
+						<Icon
+							className="h-12 text-primary-500 w-auto"
+							icon="feather:alert-circle"
+						/>
+					</div>
+					<div className="py-4 text-center">
+						<h1 className="mt-2 text-4xl font-extrabold text-gray-500 dark:text-white tracking-tight sm:text-5xl">
+							No Posts Found
+						</h1>
+						<p className="mt-4 text-sm font-medium text-gray-300 dark:text-gray-400">
+							Sorry, we couldn’t find any blog posts ¯\_(ツ)_/¯
+						</p>
+						<div className="mt-6 flex justify-center items-center space-x-4">
 							<Button.Standard
 								icon="feather:arrow-left"
 								onClick={() => history.go(-1)}
-								type={NavigationItemType.ACTION}
-							>
+								type={NavigationItemType.ACTION}>
 								Back
 							</Button.Standard>
 							<Button.Standard
 								href="/"
 								icon="feather:home"
-								type={NavigationItemType.LINK}
-							>
+								type={NavigationItemType.LINK}>
 								Home
 							</Button.Standard>
 							{routeBlog && (
 								<Button.Standard
 									href="/blog"
 									icon="feather:book"
-									type={NavigationItemType.LINK}
-								>
+									type={NavigationItemType.LINK}>
 									Blog
 								</Button.Standard>
 							)}
-						</Actions>
-					</Body>
-				</Content>
-			</Container>
+						</div>
+					</div>
+				</div>
+			</div>
 		</Layout.Error>
 	);
 }
--- components/Blog/Post.component.tsx
+++ components/Blog/Post.component.tsx
@@ -1,83 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import styled from '@emotion/styled';
-import tw from 'twin.macro';
 
 import { Pill } from '~/components';
 
 import type { FrontMatter } from '~/types';
+import clsx from 'clsx';
 
 interface PostProps {
 	index: number;
 	frontmatter: FrontMatter;
 }
-
-const Container = styled.a(tw`
-	flex flex-col \
-	bg-white bg-opacity-75 dark:bg-gray-900 dark:bg-opacity-75 \
-	backdrop-filter backdrop-blur-sm \
-	border-2 border-gray-100 dark:border-gray-500 \
-	rounded-2xl overflow-hidden hover:shadow-xl cursor-pointer \
-	transform motion-safe:hover:-translate-y-1 \
-	transition ease-in-out duration-300 \
-	focus:outline-none focus:ring-4 focus:ring-offset-8 focus:ring-primary-500
-`);
-
-const Banner = styled.div`
-	${tw`
-		relative flex justify-center w-full max-w-xl \
-		my-auto \
-		rounded-t-lg overflow-hidden
-	`}
-
-	img {
-		${tw`
-			absolute top-0 left-0 w-full h-48 \
-			object-cover select-none
-		`}
-	}
-`;
-
-const BannerPlaceholder = styled.div(tw`
-	w-full h-full lg:h-48 \
-	bg-gray-200 dark:bg-gray-600
-	motion-safe:animate-pulse
-`);
-
-const Content = styled.div<{ banner: boolean }>`
-	${tw`
-		flex-1 flex flex-col justify-between \
-		p-6 \
-		bg-transparent \
-		rounded-2xl \
-		bg-transparent
-	`}
-
-	${({ banner }) => banner && tw`lg:rounded-tr-none lg:rounded-tl-none`}
-`;
-
-const ContextText = styled.div(tw`
-	flex flex-col flex-1 justify-around \
-	rounded-lg \
-	text-gray-300 dark:text-gray-400 \
-	focus:outline-none focus:ring-4 focus:border-none focus:ring-primary-500
-`);
-
-const Title = styled.p(tw`
-	text-xl font-bold \
-	text-gray-900 dark:text-gray-100
-`);
-
-const Description = styled.p(tw`
-	mt-3 \
-	text-base line-clamp-2
-`);
-
-const Footer = styled.div(tw`
-	flex items-start space-x-1 \
-	mt-4 \
-	text-sm
-`);
 
 export function _Post({ index, frontmatter }: PostProps) {
 	const ariaLabel = `Read blog post: ${frontmatter.title}`;
@@ -85,34 +17,46 @@ export function _Post({ index, frontmatter }: PostProps) {
 
 	return (
 		<Link aria-label={ariaLabel} href={href} passHref>
-			<Container aria-label={ariaLabel} href={href}>
+			<a
+				aria-label={ariaLabel}
+				className="flex flex-col bg-white bg-opacity-75 dark:bg-gray-900 dark:bg-opacity-75 backdrop-filter backdrop-blur-sm border-2 border-gray-100 dark:border-gray-500 rounded-2xl overflow-hidden hover:shadow-xl cursor-pointer transform motion-safe:hover:-translate-y-1 default-transition default-focus"
+				href={href}>
 				{frontmatter.banner && index <= 2 && (
-					<Banner>
-						<BannerPlaceholder />
+					<div className="relative flex justify-center w-full max-w-xl my-auto rounded-t-lg overflow-hidden">
+						<div className="w-full h-full lg:h-48 bg-gray-200 dark:bg-gray-600 motion-safe:animate-pulse" />
 						<Image
 							alt={frontmatter.title}
+							className="absolute top-0 left-0 w-full h-48 object-cover select-none"
 							draggable={false}
 							layout="fill"
 							loading="lazy"
 							src={frontmatter.banner}
 						/>
-					</Banner>
+					</div>
 				)}
 
-				<Content banner={frontmatter.banner && index <= 2}>
-					<ContextText>
-						<Title>{frontmatter.title}</Title>
+				<div
+					className={clsx(
+						'flex-1 flex flex-col justify-between p-6 bg-transparent rounded-2xl bg-transparent',
+						frontmatter.banner && index <= 2 && 'lg:(rounded-tr-none rounded-tl-none)',
+					)}>
+					<div className="flex flex-col flex-1 justify-around rounded-lg text-gray-300 dark:text-gray-400 default-focus">
+						<h4 className="text-xl font-bold text-gray-900 dark:text-gray-100">
+							{frontmatter.title}
+						</h4>
 						{((frontmatter.description && frontmatter.description_show) || true) && (
-							<Description aria-label={frontmatter.description}>
+							<p
+								aria-label={frontmatter.description}
+								className="mt-3 text-base line-clamp-2">
 								{frontmatter.description}
-							</Description>
+							</p>
 						)}
-						<Footer>
+						<div className="flex items-start space-x-1 mt-4 text-sm">
 							<Pill.Date>{frontmatter.date}</Pill.Date>
-						</Footer>
-					</ContextText>
-				</Content>
-			</Container>
+						</div>
+					</div>
+				</div>
+			</a>
 		</Link>
 	);
 }
