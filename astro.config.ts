06b2d046e2bd0fa7df355994b0c55da26165bf05
nurodev <me@nuro.dev>
2022-08-17 11:03:11 +0100

    Added sitemap support

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,20 @@
+import sitemap from '@astrojs/sitemap';
 import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/edge';
 import { defineConfig } from 'astro/config';
 
+const { NODE_ENV, VERCEL_URL } = process.env;
+
+const isVercel = Boolean(NODE_ENV === 'production' && VERCEL_URL);
+
 export default defineConfig({
-	adapter: vercel(),
-	integrations: [solid(), tailwind()],
-	output: 'server',
+	site: isVercel ? (VERCEL_URL ? VERCEL_URL : 'https://nuro.dev') : 'http://localhost:3000',
+	integrations: [sitemap(), solid(), tailwind()],
+	...(isVercel
+		? {
+				adapter: vercel(),
+				output: 'server',
+		  }
+		: {}),
 });
