f6aa2941ad31023d238da24c806d1a4003ae5e0c
nurodev <me@nuro.dev>
2022-08-17 10:44:40 +0100

    Add Solid.js support

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
+import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [solid(), tailwind()],
 });
