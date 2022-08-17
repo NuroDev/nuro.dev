2cfc1de082b48ab7d4384e9f0232a66efa3e6eef
nurodev <me@nuro.dev>
2022-08-17 10:47:28 +0100

    Added Vercel (edge) adapter to Astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,10 @@
 import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/edge';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+	adapter: vercel(),
 	integrations: [solid(), tailwind()],
+	output: 'server',
 });
