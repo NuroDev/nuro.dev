c0e6181818d4b3cbe897870072616037e979bce7
nurodev <me@nuro.dev>
2022-09-14 02:21:20 +0100

    Added basic theming system

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
  */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
+	darkMode: ['class', '[data-theme="dark"]'],
 	theme: {
 		extend: {
 			animation: {
