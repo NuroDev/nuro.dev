9e5aa68a6b87b4ca3cbcf98ea7ebec90402884ef
nurodev <me@nuro.dev>
2022-11-08 13:41:02 +0000

    Added link to blog page to index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+
 export default function HomePage(): JSX.Element {
-	return <h1 className="text-2xl font-bold">Hello World</h1>;
+	return (
+		<>
+			<h1 className="text-2xl font-bold">Hello World</h1>
+
+			<Link href="/blog">Go to /blog</Link>
+		</>
+	);
 }
