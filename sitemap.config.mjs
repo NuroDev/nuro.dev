a18d8a6c030fb0c9540da0a030f98c2bc057f8d0
nurodev <me@nuro.dev>
2022-11-08 15:23:47 +0000

    Fixed sitemap config

--- sitemap.config.mjs
+++ sitemap.config.mjs
@@ -1,7 +1,5 @@
 // @ts-check
 
-import { env } from './src/env'
-
 /**
  * Don't be scared of the generics here.
  * All they do is to give us autocompletion when using this.
@@ -14,15 +12,18 @@ function defineSitemapConfig(config) {
 	return config
 }
 
-const isProduction = env.NODE_ENV === 'production';
+const isVercel = process.env.VERCEL === '1';
+const isProduction = process.env.NODE_ENV === 'production';
+
+const domain = isVercel ? process.env.VERCEL_URL : isProduction ? 'nuro.dev' : 'localhost:3000';
 const protocol = isProduction ? 'https' : 'http';
 
 export default defineSitemapConfig({
 	generateRobotsTxt: true,
 	robotsTxtOptions: {
 		additionalSitemaps: [
-			`${protocol}://${env.DOMAIN}/sitemap.xml`,
+			`${protocol}://${domain}/sitemap.xml`,
 		],
 	},
-	siteUrl: `${protocol}://${env.DOMAIN}`,
+	siteUrl: `${protocol}://${domain}`,
 })
