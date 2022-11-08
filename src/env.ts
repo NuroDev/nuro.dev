e3bdca84c2fe6004ecf4769e48eb617e52263fec
nurodev <me@nuro.dev>
2022-11-08 14:01:57 +0000

    Updated typed env vars

--- src/env.ts
+++ src/env.ts
@@ -2,12 +2,30 @@ import { envsafe, str } from 'envsafe';
 
 export const env = envsafe(
 	{
+		/**
+		 * @name DOMAIN
+		 *
+		 * @description Domain or hostname of the website
+		 */
 		DOMAIN: str({
 			allowEmpty: false,
 			default: 'nuro.dev',
+			desc: 'Domain or hostname of the website',
 			devDefault: 'localhost:3000',
 			example: 'google.com',
 			input: process.env.VERCEL === '1' ? process.env.VERCEL_URL : process.env.DOMAIN,
+		}),
+
+		/**
+		 * @name NODE_ENV
+		 *
+		 * @description Node.js runtime environment
+		 */
+		NODE_ENV: str({
+			choices: ['development', 'test', 'production'],
+			default: 'production',
+			desc: 'Node.js runtime environment',
+			devDefault: 'development',
 		}),
 	},
 	{
