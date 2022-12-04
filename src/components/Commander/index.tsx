fde70e30bb9de68a69704cb31f0d4bc5e068551b
nurodev <me@nuro.dev>
2022-12-04 12:04:07 +0000

    Replaced `motion` with native CSS animations

--- src/components/Commander/index.tsx
+++ src/components/Commander/index.tsx
@@ -1,10 +1,8 @@
 import { Combobox, Dialog, Transition } from '@headlessui/react';
 import { Fragment, useState } from 'react';
-import { motion } from '@motionone/react';
 import { useMount, useUnmount } from 'react-use';
 
 import { cn } from '~/utils/cn';
-import { defaultTransition } from '~/utils/animate';
 import { Icon } from '~/components/Icon';
 
 const people = [
@@ -34,36 +32,24 @@ export function Commander(): JSX.Element {
 
 	return (
 		<>
-			<motion.div
-				animate={{
-					opacity: [0, 1],
-					y: [100, 0],
-				}}
-				transition={{
-					...defaultTransition,
-					delay: 0.5,
-				}}
-			>
+			<div className="animate-in slide-in-from-left duration-1000">
 				<button
-					// className="flex w-full max-w-md items-center justify-between !text-gray-500 hover:!text-white"
 					className="default-transition default-focus flex w-full max-w-md items-center justify-between rounded-lg border-2 border-gray-100/50 bg-gray-100/25 px-4 py-2 backdrop-blur-md dark:border-gray-800 dark:bg-gray-900/25 dark:hover:border-gray-700 dark:hover:bg-gray-700/50"
 					onClick={(): void => setOpen(true)}
-					type="button"
-				>
+					type="button">
 					<div className="inline-flex items-center justify-center space-x-2">
 						<Icon name="Search" />
 						<span>Search</span>
 					</div>
 					<span>⌘K</span>
 				</button>
-			</motion.div>
+			</div>
 
 			<Transition.Root
 				afterLeave={(): void => setQuery('')}
 				appear={true}
 				as={Fragment}
-				show={open}
-			>
+				show={open}>
 				<Dialog as="div" className="relative z-10" onClose={setOpen}>
 					<Transition.Child
 						as={Fragment}
@@ -72,8 +58,7 @@ export function Commander(): JSX.Element {
 						enterTo="opacity-100"
 						leave="ease-in duration-200"
 						leaveFrom="opacity-100"
-						leaveTo="opacity-0"
-					>
+						leaveTo="opacity-0">
 						<div className="fixed inset-0 bg-black bg-opacity-50 transition-opacity dark:bg-opacity-75" />
 					</Transition.Child>
 
@@ -85,8 +70,7 @@ export function Commander(): JSX.Element {
 							enterTo="opacity-100 scale-100"
 							leave="ease-in duration-200"
 							leaveFrom="opacity-100 scale-100"
-							leaveTo="opacity-0 scale-95"
-						>
+							leaveTo="opacity-0 scale-95">
 							<Dialog.Panel className="mx-auto max-w-xl transform divide-y divide-gray-100 overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 backdrop-blur-md transition-all dark:border dark:border-gray-800 dark:bg-gray-800/50">
 								<Combobox
 								// onChange={(person): void => (window.location = person?.url)}
@@ -107,8 +91,7 @@ export function Commander(): JSX.Element {
 									{filteredPeople.length > 0 && (
 										<Combobox.Options
 											static
-											className="max-h-72 scroll-py-2 overflow-y-auto py-2 text-sm text-gray-800"
-										>
+											className="max-h-72 scroll-py-2 overflow-y-auto py-2 text-sm text-gray-800">
 											{filteredPeople.map((person) => (
 												<Combobox.Option
 													key={person.id}
@@ -118,8 +101,7 @@ export function Commander(): JSX.Element {
 															'cursor-default select-none px-4 py-2',
 															active && 'bg-indigo-600 text-white',
 														)
-													}
-												>
+													}>
 													{person.name}
 												</Combobox.Option>
 											))}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
-import { motion } from '@motionone/react';
-
 import { Background } from '~/components/Background';
 import { BaseLayout } from '~/layouts/Base.layout';
 import { Commander } from '~/components/Commander';
-import { defaultTransition } from '~/utils/animate';
 import { name as personName } from '~/data';
 
 export default function HomePage(): JSX.Element {
@@ -13,36 +10,18 @@ export default function HomePage(): JSX.Element {
 			<main className="flex min-h-screen items-end justify-start px-8 py-32 sm:px-16 lg:px-32">
 				<div className="w-full max-w-md text-left sm:max-w-lg md:sm:max-w-2xl lg:sm:max-w-3xl">
 					<div className="mb-4 space-y-4 lg:mb-0">
-						<motion.h4
-							animate={{
-								opacity: [0, 1],
-								x: [-100, 0],
-							}}
-							className="text-lg font-extrabold tracking-tight text-gray-700 dark:text-white sm:text-xl lg:text-2xl"
-							transition={defaultTransition}
-						>
+						<h4 className="text-lg font-extrabold tracking-tight text-gray-700 animate-in duration-1000 motion-safe:slide-in-from-left dark:text-white sm:text-xl lg:text-2xl">
 							Hey{' '}
 							<span
 								className="inline-block origin-70 animate-wave"
 								style={{
 									animationDelay: '1.25s',
-								}}
-							>
+								}}>
 								👋
 							</span>
-						</motion.h4>
+						</h4>
 
-						<motion.h1
-							animate={{
-								opacity: [0, 1],
-								x: [-100, 0],
-							}}
-							className="flex max-w-md flex-col text-5xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-6xl lg:text-9xl"
-							transition={{
-								...defaultTransition,
-								delay: 0.25,
-							}}
-						>
+						<h1 className="flex max-w-md flex-col text-5xl font-extrabold tracking-tight text-gray-900 animate-in duration-1000 delay-300 motion-safe:slide-in-from-left dark:text-white sm:text-6xl lg:text-9xl">
 							<span className="z-20">I&apos;m {personName}</span>
 
 							<div className="z-10 -mt-4 h-[0.58em] w-full max-w-[12rem] lg:-mt-8 lg:max-w-none">
@@ -51,8 +30,7 @@ export default function HomePage(): JSX.Element {
 									fill="none"
 									preserveAspectRatio="none"
 									viewBox="0 0 418 47"
-									xmlns="http://www.w3.org/2000/svg"
-								>
+									xmlns="http://www.w3.org/2000/svg">
 									<path
 										className="animate-dash stroke-primary-600"
 										d="M3.5 31C55.6044 18.119 84.5636 11.5337 138 6.5C159.039 4.51809 171.907 3.1994 193 4.5C214.093 5.8006 219 7.49999 220.5 8.5C222 9.5 219.5 14 193 23C180.691 27.1805 171.402 30.6059 164.984 33.0756C162.88 33.8851 163.543 36.7752 165.792 36.6131C182.78 35.3884 214.61 33.2961 237.5 33C268.164 32.6033 285.373 33.4573 316 35C354.35 36.9317 414 43 414 43"
@@ -65,7 +43,7 @@ export default function HomePage(): JSX.Element {
 									/>
 								</svg>
 							</div>
-						</motion.h1>
+						</h1>
 
 						<Commander />
 					</div>
