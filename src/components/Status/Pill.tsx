a0b04fd407915c7ab9c8174b3a6e968f621570c6
nurodev <me@nuro.dev>
2022-12-08 08:17:56 +0000

    Revert "Updated status pill animations"
    This reverts commit 08bc236b5241dfd9ad1c7a3e031f3a53f9baa94f.

--- src/components/Status/Pill.tsx
+++ src/components/Status/Pill.tsx
@@ -8,7 +8,7 @@ export function StatusPill(): JSX.Element | null {
 
 	return (
 		<Link
-			className="default-transition default-focus inline-flex items-center space-x-2 rounded-full border border-gray-100/50 bg-gray-100/25 py-1 px-4 text-sm font-medium text-gray-900 backdrop-blur-md animate-in duration-1000 motion-safe:fade-in dark:border-gray-800 dark:bg-gray-900/25 dark:text-white dark:hover:bg-gray-700/50"
+			className="default-transition default-focus inline-flex items-center space-x-2 rounded-full border border-gray-100/50 bg-gray-100/25 py-1 px-4 text-sm font-medium text-gray-900 backdrop-blur-md animate-in duration-1000 motion-safe:zoom-in dark:border-gray-800 dark:bg-gray-900/25 dark:text-white dark:hover:bg-gray-700/50"
 			href="/status"
 		>
 			<span>Live</span>
