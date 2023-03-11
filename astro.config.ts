24a8729956419dd9e8797ac5f8a82c0d293ffaf6
nurodev <me@nuro.dev>
2023-03-11 00:59:40 +0000

    Added Tailwind CSS integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
+import tailwind from '@astrojs/tailwind';
+
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), tailwind()],
 });
