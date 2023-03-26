309cec87c5c2845efef44cae788a427bd644b9f6
nurodev <me@nuro.dev>
2023-03-26 00:16:08 +0000

    Updated Next.js types

--- src/types/next.ts
+++ src/types/next.ts
@@ -1,3 +1,82 @@
+import type { NextResponse } from 'next/server';
+import type { ReactNode } from 'react';
+
+import type { ArrayElement, Awaitable, UnwrapPromise } from './util';
+
+type ParamsValue = string | number | boolean;
+
+export interface NextErrorPageProps {
+	/**
+	 * **Error**
+	 *
+	 * An instance of an Error object. This error can happen on the server or the client.
+	 */
+	error: Error;
+	/**
+	 * **Reset**
+	 *
+	 * A function to reset the error boundary, which does not return a response.
+	 */
+	reset: () => void;
+}
+
+export interface NextHeadProps<TParams = Record<string, ParamsValue>> {
+	/**
+	 * **Params** (Optional)
+	 */
+	params?: TParams;
+}
+
+export type NextHeadComponent<TParams = Record<string, ParamsValue>> = (
+	props: NextHeadProps<TParams>,
+) => Awaitable<JSX.Element>;
+
+export interface NextLayoutProps<TParams = Record<string, ParamsValue>> {
+	/**
+	 * **Children**
+	 *
+	 * React component containing the route segments the layout is wrapping.
+	 */
+	children: ReactNode;
+
+	/**
+	 * **Params** (Optional)
+	 *
+	 * The dynamic route parameters object from the root segment down to that layout.
+	 */
+	params?: TParams;
+}
+
+type GenerateStaticParamsFn<TParams extends Array<unknown>> = () => TParams | Promise<TParams>;
+
+export interface NextPageProps<
+	TGenerateStaticParams extends GenerateStaticParamsFn<TParams> | undefined = undefined,
+	TParams extends Array<unknown> = Array<unknown>,
+> {
+	/**
+	 * **params** (Optional)
+	 *
+	 * The dynamic route params object from the root segment down that to that page.
+	 */
+	params: TGenerateStaticParams extends GenerateStaticParamsFn<TParams>
+		? ArrayElement<UnwrapPromise<ReturnType<TGenerateStaticParams>>>
+		: Record<string, ParamsValue> | undefined;
+
+	/**
+	 * **searchParams** (Optional)
+	 *
+	 * The URL search params object.
+	 *
+	 * @example `acme.com/?q=cat` → `{ q: "cat" }`
+	 */
+	searchParams?: Record<string, ParamsValue>;
+}
+
+export type NextRouteHandler<
+	TParams extends object = Record<string, unknown>,
+	TResponse extends Response = NextResponse,
+> = (request: Request, params: { params?: TParams }) => Awaitable<TResponse>;
+
 type Region =
 	| 'arn1'
 	| 'bom1'
@@ -19,6 +98,6 @@ type Region =
 	| 'syd1';
 
 export interface NextApiConfig {
-	runtime?: 'experimental-edge' | 'nodejs';
+	runtime?: 'edge' | 'nodejs';
 	regions?: Array<Region>;
 }
