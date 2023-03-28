5294f8e5a434599850c8a8f0b8c3c85891707109
nurodev <me@nuro.dev>
2023-03-28 17:45:04 +0100

    Updated initial page structure

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,17 +1,15 @@
-import Link from 'next/link';
-
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-	title: 'status ─ nuro',
+	title: 'blog ─ nuro',
 };
 
-export default function RootPage(): JSX.Element {
+export default function BlogPage(): JSX.Element {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
 				<h1 className="z-10 text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-					Current <span className="text-primary-600">Status</span>
+					Hello <span className="text-primary-600">Blog</span>
 				</h1>
 			</div>
 		</main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 
-import type { UrlObject } from 'url';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -8,18 +7,18 @@ export const metadata: Metadata = {
 };
 
 interface CardProps {
-	description: string;
-	href: UrlObject | __next_route_internal_types__.RouteImpl<string>;
+	description?: string;
+	href: __next_route_internal_types__.RouteImpl<string>;
 	title: string;
 }
 
-function Card({ description, href, title }: CardProps): JSX.Element {
+function Card({ href, title }: CardProps): JSX.Element {
 	return (
 		<Link
-			className="default-focus default-transition flex max-w-xs flex-col gap-4 rounded-xl border border-gray-100 bg-gray-800/50 p-4 text-white backdrop-blur-sm backdrop-filter hover:bg-gray-700/50 dark:border-gray-600"
+			className="default-focus default-transition flex w-64 flex-col gap-2 rounded-xl border border-gray-100 bg-gray-800/50 p-4 text-white backdrop-blur-sm backdrop-filter hover:bg-gray-700/50 dark:border-gray-600"
 			href={href}>
 			<h3 className="text-2xl font-bold">{title}</h3>
-			<div className="text-lg">{description}</div>
+			<div className="text-sm text-gray-400">{href}</div>
 		</Link>
 	);
 }
@@ -29,14 +28,11 @@ export default function RootPage(): JSX.Element {
 		<main className="flex min-h-screen flex-col items-center justify-center">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
 				<h1 className="z-10 text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-					Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+					Create <span className="text-primary-600">T3</span> App
 				</h1>
 				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-					<Card
-						title="Status →"
-						description="Just the basics - Everything you need to know to set up your database and authentication."
-						href="/status"
-					/>
+					<Card title="Blog" href="/blog" />
+					<Card title="Status" href="/status" />
 				</div>
 			</div>
 		</main>
