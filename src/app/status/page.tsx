68da8cab34a7c0ba5b6a3ec0d5dd145807d6d832
nurodev <me@nuro.dev>
2023-03-28 17:36:28 +0100

    Added initial basic status page

---
+++ src/app/status/page.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'status ─ nuro',
+};
+
+export default function RootPage(): JSX.Element {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center">
+			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+				<h1 className="z-10 text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+					Current <span className="text-[hsl(280,100%,70%)]">Status</span>
+				</h1>
+			</div>
+		</main>
+	);
+}
