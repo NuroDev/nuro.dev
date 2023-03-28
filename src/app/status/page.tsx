ca1e0d8ba8737b5d4b8b66d25401cfde15947f11
nurodev <me@nuro.dev>
2023-03-28 23:54:03 +0100

    Minor status page tweaks

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -1,12 +1,10 @@
-import Link from 'next/link';
-
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
 	title: 'status',
 };
 
-export default function RootPage(): JSX.Element {
+export default function StatusPage(): JSX.Element {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
