ec2c2eb9bf726d74493d17d48be95ec73fd05c37
nurodev <me@nuro.dev>
2023-03-28 18:07:57 +0100

    Theme fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ interface CardProps {
 function Card({ href, title }: CardProps): JSX.Element {
 	return (
 		<Link
-			className="default-focus default-transition flex w-64 flex-col gap-2 rounded-xl border border-gray-100 bg-gray-800/50 p-4 text-white backdrop-blur-sm backdrop-filter hover:bg-gray-700/50 dark:border-gray-600"
+			className="default-focus default-transition flex w-64 flex-col gap-2 rounded-xl border border-gray-100 bg-gray-100/50 p-4 backdrop-blur-sm backdrop-filter hover:bg-gray-200/50 dark:border-gray-600 dark:bg-gray-800/50 dark:text-white dark:hover:bg-gray-700/50"
 			href={href}>
 			<h3 className="text-2xl font-bold">{title}</h3>
 			<div className="text-sm text-gray-400">{href}</div>
@@ -27,7 +27,7 @@ export default function RootPage(): JSX.Element {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-				<h1 className="z-10 text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+				<h1 className="z-10 text-5xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-[5rem]">
 					Create <span className="text-primary-600">T3</span> App
 				</h1>
 				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
