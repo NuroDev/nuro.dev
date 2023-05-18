20722f744d27fdf83be3ce00937fc47158273669
nurodev <me@nuro.dev>
2023-05-19 00:25:12 +0100

    Minor outline button border style fix

--- src/components/Button/Outline.component.tsx
+++ src/components/Button/Outline.component.tsx
@@ -26,7 +26,7 @@ export function Outline({
 		<Link href={href} passHref>
 			<a
 				className={clsx(
-					'inline-flex items-center justify-center w-full sm:w-auto bg-gray-50/75 hover:bg-gray-100/75 hover:text-gray-500 dark:bg-gray-900/75 dark:hover:bg-gray-800/75 dark:border-gray-700 dark:text-primary-500 dark:hover:text-primary-400 backdrop-filter backdrop-blur-sm saturate-200 text-gray-400 font-medium border-2 border-gray-200 rounded-lg cursor-pointer default-transition default-focus',
+					'inline-flex items-center justify-center w-full sm:w-auto bg-gray-50/75 hover:bg-gray-100/75 hover:text-gray-500 dark:bg-gray-900/75 dark:hover:bg-gray-800/75 dark:border-gray-700 dark:text-primary-500 dark:hover:text-primary-400 backdrop-filter backdrop-blur-sm saturate-200 text-gray-400 font-medium border-2 border-gray-200/50 rounded-lg cursor-pointer default-transition default-focus',
 					small ? 'px-4 py-1 text-sm' : 'px-8 py-2',
 					className,
 				)}
@@ -34,8 +34,7 @@ export function Outline({
 				onClick={(...args): void => onClick && onClick(...args)}
 				rel="noopener noreferrer"
 				target={external ? '_blank' : undefined}
-				{...rest}
-			>
+				{...rest}>
 				{icon && <Icon className="mt-1 mr-3" icon={icon} />}
 				{children}
 			</a>
