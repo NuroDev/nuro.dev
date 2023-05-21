5e09aae6ef9da10affbf4fa80d473f95b03d4a30
nurodev <me@nuro.dev>
2023-05-21 01:36:38 +0100

    Fixed notes page SSG file paths

--- src/app/notes/[slug]/page.tsx
+++ src/app/notes/[slug]/page.tsx
@@ -4,13 +4,16 @@ import { join } from 'node:path';
 import type { NextPageProps } from '~/types/next';
 
 export async function generateStaticParams() {
-	const allPostSlugs = await readdir(join(process.cwd(), 'src', 'data', 'notes'))
+	const allPostSlugs = await readdir(join(process.cwd(), 'src', 'data', 'notes'));
 
-	return allPostSlugs.map((slug) => ({ slug }))
-  }
+	return allPostSlugs.map((slug) => ({ slug }));
+}
 
 export default async function Post({ params }: NextPageProps<typeof generateStaticParams>) {
-	const note = await readFile(join(process.cwd(), 'src', 'data', 'notes', `${params.slug}.md`), 'utf-8');
+	const note = await readFile(
+		join(process.cwd(), 'src', 'data', 'notes', `${params.slug}`),
+		'utf-8',
+	);
 
 	return (
 		<main className="flex min-h-screen items-center justify-center py-12">
