4856ac60de245e3316d0df3247ff5bca11ee0575
nurodev <me@nuro.dev>
2023-05-21 02:18:50 +0100

    Set `/notes` to force SSG / static

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -3,6 +3,8 @@ import { readdir } from 'node:fs/promises';
 import { join, parse as parsePath } from 'node:path';
 import type { NextPageProps } from '~/types/next';
 
+export const dynamic = 'force-static';
+
 export default async function Posts(_props: NextPageProps): Promise<JSX.Element> {
 	const postFilePath = join(process.cwd(), 'src', 'data', 'notes');
 	const allPostSlugs = await readdir(postFilePath).then((paths) =>
