55d9e8957ea2f94c30bad4aa8daf917dc0092658
nurodev <me@nuro.dev>
2023-05-21 01:51:51 +0100

    Added `@trivago/prettier-plugin-sort-imports` Prettier plugin

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
 	bracketSpacing: true,
 	importOrderSeparation: true,
 	plugins: [
+		require.resolve("@trivago/prettier-plugin-sort-imports"),
 		require.resolve("prettier-plugin-tailwindcss"),
 	],
 	printWidth: 100,
