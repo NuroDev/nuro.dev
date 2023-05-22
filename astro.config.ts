8ff64765954e65917d76eae53f97d8f6e6406813
nurodev <me@nuro.dev>
2023-05-22 13:06:16 +0100

    Switched to server output (Vercel or Node.js)

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,24 @@
+import node from '@astrojs/node';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/edge';
+import type { AstroIntegration } from 'astro';
 import { defineConfig } from 'astro/config';
 
+function getAdapter(): AstroIntegration | undefined {
+	if (import.meta.env.DEV) return undefined;
+
+	if (import.meta.env.VERCEL === '1') return vercel();
+
+	return node({
+		mode: 'standalone',
+	});
+}
+
 export default defineConfig({
+	// For some reason Astro does not allow passing `undefined` here
+	// so we need to assert the return type of `getAdapter` to `AstroIntegration`
+	adapter: getAdapter() as AstroIntegration,
 	integrations: [solidJs(), tailwind()],
+	output: 'server',
 });
