567f2fd00c608e133c7c9a86a87565e7eae07a50
nurodev <me@nuro.dev>
2023-05-26 01:29:41 +0100

    Added basic click audio handler

---
+++ src/components/Click.tsx
@@ -0,0 +1,19 @@
+import { makeAudioPlayer } from '@solid-primitives/audio';
+import { onCleanup, onMount } from 'solid-js';
+
+export function Click(): null {
+	const { play, setVolume } = makeAudioPlayer('/sounds/click.ogg');
+
+	onMount((): void => {
+		setVolume(0.25);
+		document.addEventListener('mousedown', play);
+		document.addEventListener('mouseup', play);
+	});
+
+	onCleanup((): void => {
+		document.removeEventListener('mousedown', play);
+		document.removeEventListener('mouseup', play);
+	});
+
+	return null;
+}
