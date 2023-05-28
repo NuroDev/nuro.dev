bb814e9f9d813dc5d26a4eb2ed0e8c5caaa5e813
nurodev <me@nuro.dev>
2023-05-28 23:26:07 +0100

    Added initial remark & rehype plugins

--- astro.config.ts
+++ astro.config.ts
@@ -4,9 +4,16 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/edge';
 import type { AstroIntegration } from 'astro';
 import { defineConfig } from 'astro/config';
+import RehypeAutolinkHeadings from 'rehype-autolink-headings';
+import RemarkCodeTitles from 'remark-code-titles';
+import RemarkEmoji from 'remark-emoji';
+import RemarkPrism from 'remark-prism';
+import RemarkSlug from 'remark-slug';
 
-function getAdapter(): AstroIntegration | undefined {
-	if (import.meta.env.DEV) return undefined;
+function getAdapter(): AstroIntegration {
+	// For some reason Astro does not allow passing `undefined` here
+	// so we need to assert the return type of `getAdapter` to `AstroIntegration`
+	if (import.meta.env.DEV) return undefined as unknown as AstroIntegration;
 
 	// TODO: Add `VERCEL === '1'` check & default to Node.js server
 
@@ -16,9 +23,11 @@ function getAdapter(): AstroIntegration | undefined {
 }
 
 export default defineConfig({
-	// For some reason Astro does not allow passing `undefined` here
-	// so we need to assert the return type of `getAdapter` to `AstroIntegration`
-	adapter: getAdapter() as AstroIntegration,
+	adapter: getAdapter(),
 	integrations: [prefetch(), solidJs(), tailwind()],
+	markdown: {
+		rehypePlugins: [[RehypeAutolinkHeadings, {}]],
+		remarkPlugins: [RemarkCodeTitles, RemarkEmoji, RemarkPrism, RemarkSlug],
+	},
 	output: 'server',
 });
