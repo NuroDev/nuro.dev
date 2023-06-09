f2e3c89475717415c615a29721c62998cd8caa3e
nurodev <me@nuro.dev>
2023-06-09 11:55:45 +0100

    Updated Astro adapter

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,6 @@ import prefetch from '@astrojs/prefetch';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/edge';
-import type { AstroIntegration } from 'astro';
 import { defineConfig } from 'astro/config';
 import RehypeAutolinkHeadings from 'rehype-autolink-headings';
 import RemarkCodeTitles from 'remark-code-titles';
@@ -11,21 +10,12 @@ import RemarkEmoji from 'remark-emoji';
 import RemarkPrism from 'remark-prism';
 import RemarkSlug from 'remark-slug';
 
-function getAdapter(): AstroIntegration {
-	// For some reason Astro does not allow passing `undefined` here
-	// so we need to assert the return type of `getAdapter` to `AstroIntegration`
-	if (import.meta.env.DEV) return undefined as unknown as AstroIntegration;
-
-	// TODO: Add `VERCEL === '1'` check & default to Node.js server
-
-	return vercel({
-		analytics: true,
-	});
-}
-
 // https://astro.build/config
 export default defineConfig({
-	adapter: getAdapter(),
+	adapter: vercel({
+		analytics: true,
+	}),
+	experimental: {},
 	integrations: [
 		Image({
 			serviceEntryPoint: '@astrojs/image/sharp',
